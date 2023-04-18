bc1bf4f00528ca53dd41e352cd6fe957e554f69a
cjfal2 <cjfal2@kakao.com>
2023-04-18 22:38:18 +0900

    Feat: frontend Flutter 폴더 구조 생성 및 로그인화면만 구성

---
+++ frontend/hearo_app/lib/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("임시")),
+      body: const Text("임시바디"),
+    );
+  }
+}
