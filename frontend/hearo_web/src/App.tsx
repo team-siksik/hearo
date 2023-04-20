d8ff4fffaf50e769e47bec8b113e35fbe7bb3c99
isabel-noh <nohhyunjeong93@gmail.com>
2023-04-20 14:30:59 +0900

    [S08P31A603-126] feat: env-setting-web done

--- frontend/hearo_web/src/App.tsx
+++ frontend/hearo_web/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -14,7 +14,7 @@ function App() {
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-                <div>되돌릴수있어</div>
+          <div>되돌릴수있어</div>
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -30,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
