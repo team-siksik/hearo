85b128c0f59e730aa128b250b0a594de0822d08d
cjfal2 <cjfal2@kakao.com>
2023-04-21 01:07:09 +0900

    Feat: 세팅 스크린 라우터 형성

--- frontend/hearo_app/lib/screens/mysettings/setting_home.dart
+++ frontend/hearo_app/lib/screens/mysettings/setting_home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hearo_app/screens/mysettings/favorite_say.dart';
+import 'package:hearo_app/screens/mysettings/setting_screen.dart';
 import 'package:hearo_app/widgets/common/custom_app_bar_inner.dart';
 
 class SettingHome extends StatelessWidget {
@@ -21,65 +24,75 @@ class SettingHome extends StatelessWidget {
               padding: EdgeInsets.only(top: 10, bottom: 25),
               child: Text("kimnaboog12@gmail.com",
                   style: TextStyle(fontSize: 16))),
-          Container(
-            width: size.width * 0.9,
-            padding: EdgeInsets.symmetric(vertical: 22),
-            decoration: BoxDecoration(
-                border:
-                    Border(top: BorderSide(color: Colors.black12, width: 1))),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(Icons.book_outlined),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15),
-                        child: Text("자주 쓰는 말",
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w600)),
-                      )
-                    ],
-                  ),
-                  Icon(Icons.arrow_forward_ios_rounded)
-                ],
+          GestureDetector(
+            onTap: () {
+              Get.to(() => FavoriteSay());
+            },
+            child: Container(
+              width: size.width * 0.9,
+              padding: EdgeInsets.symmetric(vertical: 22),
+              decoration: BoxDecoration(
+                  border:
+                      Border(top: BorderSide(color: Colors.black12, width: 1))),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(Icons.book_outlined),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Text("자주 쓰는 말",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.w600)),
+                        )
+                      ],
+                    ),
+                    Icon(Icons.arrow_forward_ios_rounded)
+                  ],
+                ),
               ),
             ),
           ),
-          Container(
-            width: size.width * 0.9,
-            padding: EdgeInsets.symmetric(vertical: 22),
-            decoration: BoxDecoration(
-                border:
-                    Border(top: BorderSide(color: Colors.black12, width: 1))),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.settings_outlined,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15),
-                        child: Text("환경 설정",
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w600)),
-                      )
-                    ],
-                  ),
-                  Icon(Icons.arrow_forward_ios_rounded)
-                ],
+          GestureDetector(
+            onTap: () {
+              Get.to(() => SettingScreen());
+            },
+            child: Container(
+              width: size.width * 0.9,
+              padding: EdgeInsets.symmetric(vertical: 22),
+              decoration: BoxDecoration(
+                  border:
+                      Border(top: BorderSide(color: Colors.black12, width: 1))),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.settings_outlined,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Text("환경 설정",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.w600)),
+                        )
+                      ],
+                    ),
+                    Icon(Icons.arrow_forward_ios_rounded)
+                  ],
+                ),
               ),
             ),
           ),
@@ -125,9 +138,61 @@ class SettingHome extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  // TODO : 컨펌 달기
-                  GestureDetector(
-                    onTap: () {},
+                  TextButton(
+                    onPressed: () => showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text(
+                          '로그아웃',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        content: const Text(
+                          '로그아웃 하시겠습니까?',
+                          textAlign: TextAlign.center,
+                        ),
+                        actions: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStatePropertyAll(
+                                        Color.fromARGB(255, 255, 255, 255)),
+                                    shape: MaterialStatePropertyAll(
+                                        RoundedRectangleBorder(
+                                            side: BorderSide(
+                                                color: Colors.black38),
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(30))))),
+                                onPressed: () {
+                                  Get.back();
+                                },
+                                child: const Text(
+                                  '돌아가기',
+                                  style: TextStyle(color: Colors.black38),
+                                ),
+                              ),
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStatePropertyAll(
+                                        Color(0xffe63e43)),
+                                    shape: MaterialStatePropertyAll(
+                                        RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(30))))),
+                                onPressed: () {
+                                  Get.back();
+                                },
+                                child: const Text(
+                                  '로그아웃',
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                     child: Text(
                       "로그아웃",
                       style: TextStyle(
@@ -136,8 +201,72 @@ class SettingHome extends StatelessWidget {
                           fontWeight: FontWeight.w500),
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () {},
+                  TextButton(
+                    onPressed: () => showDialog(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text(
+                          '회원탈퇴',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        content: SizedBox(
+                          height: size.height * 0.1,
+                          child: Column(
+                            children: [
+                              const Text(
+                                '정말로 탈퇴 하시겠습니까?',
+                                textAlign: TextAlign.center,
+                              ),
+                              const Text(
+                                '정보 복구가 어려울 수 있습니다.',
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
+                        ),
+                        actions: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStatePropertyAll(
+                                        Color.fromARGB(255, 255, 255, 255)),
+                                    shape: MaterialStatePropertyAll(
+                                        RoundedRectangleBorder(
+                                            side: BorderSide(
+                                                color: Colors.black38),
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(30))))),
+                                onPressed: () {
+                                  Get.back();
+                                },
+                                child: const Text(
+                                  '돌아가기',
+                                  style: TextStyle(color: Colors.black38),
+                                ),
+                              ),
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStatePropertyAll(
+                                        Color(0xffe63e43)),
+                                    shape: MaterialStatePropertyAll(
+                                        RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(30))))),
+                                onPressed: () {
+                                  Get.back();
+                                },
+                                child: const Text(
+                                  '탈퇴하기',
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
                     child: Text(
                       "회원 탈퇴",
                       style: TextStyle(
