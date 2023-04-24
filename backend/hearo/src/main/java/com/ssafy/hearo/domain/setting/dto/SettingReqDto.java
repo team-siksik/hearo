4615d41ef67b0aa57fa4eec7996278f621fa38ed
nayeonkinn <nayeonkinn@gmail.com>
2023-04-24 17:19:53 +0900

    [S08P31A603-251] Feat: setting 조회 api 구현 (초안)

---
+++ backend/hearo/src/main/java/com/ssafy/hearo/domain/setting/dto/SettingReqDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.hearo.domain.setting.dto;
+
+import lombok.Getter;
+
+
+public class SettingReqDto {
+
+    @Getter
+    public static class GetSettingInfoDto {
+        private long userSeq;
+    }
+
+}
