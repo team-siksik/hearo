647fbd23f33abc163138378ffb47b417e1a9c9d0
gs20wow <gs20wow@gmail.com>
2023-04-26 15:37:44 +0900

    [S08P31A603-293] Style: 단순수정

--- frontend/hearo_web/postcss.config.cjs
+++ frontend/hearo_web/postcss.config.cjs
@@ -1,12 +1,3 @@
-// // postcss.config.js
-// module.exports = {
-//   plugins: {
-//     'postcss-import': {},
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   }
-// }
-
 module.exports = {
   plugins: {
     tailwindcss: {},
