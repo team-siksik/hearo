155922cc0e20809298d8d4b8dc7873cfa75c5910
nayeonkinn <nayeonkinn@gmail.com>
2023-04-26 12:18:02 +0900

    [S08P31A603-254] Feat: setting 수정 api 구현

--- backend/hearo/src/main/java/com/ssafy/hearo/domain/setting/service/impl/SettingServiceImpl.java
+++ backend/hearo/src/main/java/com/ssafy/hearo/domain/setting/service/impl/SettingServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ssafy.hearo.domain.setting.service.impl;
 
 import com.ssafy.hearo.domain.account.entity.Account;
+import com.ssafy.hearo.domain.setting.dto.SettingReqDto.*;
 import com.ssafy.hearo.domain.setting.dto.SettingResDto.*;
 import com.ssafy.hearo.domain.setting.entity.Setting;
 import com.ssafy.hearo.domain.setting.repository.SettingRepository;
@@ -12,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -24,18 +26,38 @@ public class SettingServiceImpl implements SettingService {
     @Override
     public SettingInfoResDto getSetting(Account account) {
         log.info("[getSetting] 설정 조회 시작");
-        long userSeq = account.getUserSeq();
-        Setting setting = settingRepository.findById(userSeq)
+
+        Setting setting = settingRepository.findById(account.getUserSeq())
                         .orElseThrow(() -> new ErrorException(CommonErrorCode.BAD_REQUEST));
+
         log.info("[getSetting] 설정 조회 완료");
+
         return SettingInfoResDto.builder()
                 .settingSeq(setting.getSettingSeq())
-                .userSeq(userSeq)
+                .userSeq(setting.getAccount().getUserSeq())
                 .wordSize(setting.getWordSize())
                 .voiceSetting(setting.getVoiceSetting())
                 .darkMode(setting.getDarkMode())
                 .mainTheme(setting.getMainTheme())
                 .build();
     }
+
+    @Override
+    public void modifySetting(Account account, ModifySettingRequestDto requestDto) {
+        log.info("[modifySetting] 설정 수정 시작");
+
+        Setting setting = settingRepository.findById(account.getUserSeq())
+                .orElseThrow(() -> new ErrorException(CommonErrorCode.BAD_REQUEST));
+
+        byte wordSize = requestDto.getWordSize();
+        byte voiceSetting = requestDto.getVoiceSetting();
+        byte darkMode = requestDto.getDarkMode();
+        String mainTheme = requestDto.getMainTheme();
+
+        setting.modify(wordSize, voiceSetting, darkMode, mainTheme);
+
+        log.info("[modifySetting] 설정 수정 완료");
+    }
+
 
 }
