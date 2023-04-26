b63ed312d8387c71c267db883f59b2bf6c0841d5
nohhyunjeong93 <nohhyunjeong93@mgmail.com>
2023-04-26 15:33:45 +0900

    [S08P31A603-207]google oauth2로그인 view 및 redirect_uri 연결

--- frontend/hearo_web/src/App.tsx
+++ frontend/hearo_web/src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Google from "./apis/google";
+import Login from "./pages/Login";
+import Communication from "./pages/Communication";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  function login(): void {
+    console.log("login");
+  }
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-          <div>되돌릴수있어</div>
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/comm" element={<Communication />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/login/oauth2/code/google" element={<Google />} />
+      </Routes>
     </div>
   );
 }
