bb3fd6973b7c366578ae0a591c05130b78c653e1
cjfal2 <cjfal2@kakao.com>
2023-04-27 00:34:30 +0900

    [S08P31A603-000] Feat: 채팅 구현
    상세설명
    - 채팅 치면 들어감

--- frontend/hearo_app/lib/screens/chats/chat_home.dart
+++ frontend/hearo_app/lib/screens/chats/chat_home.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hearo_app/widgets/chats/custom_app_bar_chat.dart';
 import 'package:hearo_app/widgets/chats/speech_bubble.dart';
@@ -12,11 +14,20 @@ class ChatHome extends StatefulWidget {
 
 class _ChatHomeState extends State<ChatHome> {
   final _scrollController = ScrollController();
+  List chattings = [];
+  String myChat = '';
+  TextEditingController textController = TextEditingController();
 
-  void addChat() {
+  void addChat(chat) {
     // 새로운 항목을 ListView에 추가합니다.
+
     setState(() {
-      conversations.add({"who": 1, "message": "회의를 시작하겠습니다."});
+      // 말풍선 확인을 위한 랜덤요소 추가
+      var random = Random();
+      var randomNumber = random.nextInt(4);
+      chattings.add({"who": randomNumber, "message": chat});
+      // chattings.add({"who": 0, "message": chat});
+      myChat = '';
     });
 
     // ListView를 맨 하단으로 스크롤합니다.
@@ -36,27 +47,6 @@ class _ChatHomeState extends State<ChatHome> {
     Wakelock.disable();
     super.dispose();
   }
-
-  List conversations = [
-    {"who": 1, "message": "회의를 시작하겠습니다회의를 시작하겠습니다회의를 시작하겠습니다."},
-    {"who": 2, "message": "안녕하세요."},
-    {"who": 0, "message": "반갑습니다!"},
-    {"who": 4, "message": "좋은 하루에요!"},
-    {"who": 5, "message": "봄바람 휘날리며~"},
-    {"who": 3, "message": "이 회의를 끝내러 왔다."},
-    {"who": 1, "message": "회의를 시작하겠습니다."},
-    {"who": 2, "message": "안녕하세요."},
-    {"who": 0, "message": "반갑습니다!"},
-    {"who": 4, "message": "좋은 하루에요!"},
-    {"who": 5, "message": "봄바람 휘날리며~"},
-    {"who": 3, "message": "이 회의를 끝내러 왔다."},
-    {"who": 1, "message": "회의를 시작하겠습니다."},
-    {"who": 2, "message": "안녕하세요."},
-    {"who": 0, "message": "반갑습니다!"},
-    {"who": 4, "message": "좋은 하루에요!"},
-    {"who": 5, "message": "봄바람 휘날리며~"},
-    {"who": 3, "message": "이 회의를 끝내러 왔다."},
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -82,13 +72,13 @@ class _ChatHomeState extends State<ChatHome> {
               child: Stack(
                 children: <Widget>[
                   Container(
-                    padding: EdgeInsets.only(bottom: 70),
+                    padding: EdgeInsets.only(bottom: 130),
                     child: ListView.separated(
                       controller: _scrollController,
                       separatorBuilder: (context, index) => SizedBox(height: 5),
-                      itemCount: conversations.length,
+                      itemCount: chattings.length,
                       itemBuilder: (context, index) {
-                        var saying = conversations[index];
+                        var saying = chattings[index];
                         return SpeechBubble(
                             message: saying["message"],
                             who: saying["who"],
@@ -100,29 +90,60 @@ class _ChatHomeState extends State<ChatHome> {
                     left: 0,
                     right: 0,
                     bottom: 0,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: '대화를 입력해주세요.',
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                          borderSide:
-                              BorderSide(width: 1, color: Color(0xffe63e43)),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                          borderSide: BorderSide(
-                              width: 1,
-                              color: const Color.fromARGB(255, 172, 172, 172)),
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                    child: GestureDetector(
+                      onTap: () {
+                        print("adsfasdfads");
+                        _scrollController
+                            .jumpTo(_scrollController.position.maxScrollExtent);
+                      },
+                      child: TextField(
+                        undoController: UndoHistoryController(
+                            value:
+                                UndoHistoryValue(canUndo: true, canRedo: true)),
+                        onTap: () {
+                          _scrollController.jumpTo(
+                              _scrollController.position.maxScrollExtent);
+                        },
+                        controller: textController,
+                        onSubmitted: (value) {
+                          addChat(value);
+                          setState(() {
+                            myChat = '';
+                            textController.text = '';
+                          });
+                        },
+                        onChanged: (text) {
+                          setState(() {
+                            myChat = text;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          hintText: '대화를 입력해주세요.',
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide:
+                                BorderSide(width: 1, color: Color(0xffe63e43)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                            borderSide: BorderSide(
+                                width: 1,
+                                color:
+                                    const Color.fromARGB(255, 172, 172, 172)),
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0)),
+                          ),
                         ),
                       ),
                     ),
                   ),
                   Positioned(
                       right: 0,
-                      bottom: 65,
+                      bottom: 70,
                       child: GestureDetector(
                         onTap: () {},
                         child: Container(
