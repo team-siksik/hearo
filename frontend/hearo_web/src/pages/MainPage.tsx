01d3bc6e315909a5465db20d9b2e383c029e73f7
gs20wow <gs20wow@gmail.com>
2023-04-27 14:57:35 +0900

    [S08P31A603-293] Style : isTopOfpage 제거
    상세설명
    - 필요없어서 제거함

--- frontend/hearo_web/src/pages/MainPage.tsx
+++ frontend/hearo_web/src/pages/MainPage.tsx
@@ -1,39 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import Navbar from "@/components/common/Navbar/Navbar";
-import MyPage from "@/pages/MyPage";
-import RecordsPage from "@/pages/RecordsPage";
-import CommunicationPage from "@/pages/CommunicationPage";
 import { SelectedPage } from "@/types/types";
 
 
 // 캐러셀 적용해야하하고
 // 로고누르면 메인으로 이동
+
+
+
 function MainPage() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.CommunicationPage);
-  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY === 0) {
-        setIsTopOfPage(true);
-        setSelectedPage(SelectedPage.CommunicationPage);
-      }
-      if (window.scrollY !== 0) setIsTopOfPage(false);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-    
     return (
-      <div>
-        <Navbar
-          isTopOfPage={isTopOfPage}
+      <div className="bg-gray-600">
+      <Navbar
           selectedPage={selectedPage}
           setSelectedPage={setSelectedPage}
         />
-        <div className="top-20">
-          메인페이지 해줘~~~ 
-        </div>
+      <div>
+        메인페이지 해줘~~~ 
+      </div>
       </div>
       )
   } 
