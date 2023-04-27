4ea97cbf2b8f40488f26cf1e84ff5ec5098d504c
isabel-noh <nohhyunjeong93@gmail.com>
2023-04-27 17:56:27 +0900

    fix: merge

--- frontend/hearo_web/src/components/index.ts
+++ frontend/hearo_web/src/components/index.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import CommunicationComp from "./Communication/CommunicationComp";
-import CommunicationStart from "./Communication/CommunicationStart";
-import Button from "./common/Button";
-import Navbar from "./common/Navbar/Navbar";
-
-export default { 
-  CommunicationComp, 
-  CommunicationStart,
-  Button,
-  Navbar, 
-=======
 import ConversationComp from "./Conversation/ConversationComp";
 import ConversationInfo from "./Conversation/ConversationInfo";
 import ConversationHeader from "./Conversation/ConversationHeader";
@@ -22,6 +10,8 @@ import Timer from "./common/Timer";
 import Modal from "./common/ui/Modal";
 import Button from "./common/ui/Button";
 import Input from "./common/ui/Input";
+import Navbar from "./common/Navbar/Navbar";
+
 
 export {
   ConversationComp,
@@ -36,5 +26,5 @@ export {
   Modal,
   Button,
   Input,
->>>>>>> dcb5dbe80d81c49e0e463d5a68efedf6710260b2
+  Navbar,
 };
