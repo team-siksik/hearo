39e9b405b9bfb6c66f818f1dfaaa6f785bdada0b
nayeonkinn <nayeonkinn@gmail.com>
2023-04-28 10:39:11 +0900

    [S08P31A603-278] Update: ErrorCode 한국어 수정

--- backend/hearo/src/main/java/com/ssafy/hearo/global/error/code/CommonErrorCode.java
+++ backend/hearo/src/main/java/com/ssafy/hearo/global/error/code/CommonErrorCode.java
@@ -10,9 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum CommonErrorCode implements ErrorCode {
 
     BAD_REQUEST("CM_000", "잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
-    INVALID_PARAMETER("CM_001", "Invalid parameter included",HttpStatus.BAD_REQUEST),
-    RESOURCE_NOT_FOUND("CM_002", "Resource not exists", HttpStatus.NOT_FOUND),
-    INTERNAL_SERVER_ERROR("CM_003", "Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
+    INVALID_PARAMETER("CM_001", "유효하지 않은 파라미터입니다.",HttpStatus.BAD_REQUEST),
+    RESOURCE_NOT_FOUND("CM_002", "리소스가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
+    INTERNAL_SERVER_ERROR("CM_003", "서버 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final String code;
     private final String message;
