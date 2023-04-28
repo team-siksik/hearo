f5f6cdd681f941ff2f85b40adb374b096eb0e607
cjfal2 <cjfal2@kakao.com>
2023-04-28 17:39:13 +0900

    [S08P31A603-211] Feat: 로그인 토큰 얻기 성공
    상세설명
    - 파이어 베이스와 구글 오쓰 토큰 얻기 성공

--- frontend/hearo_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/hearo_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import firebase_analytics
+import firebase_auth
+import firebase_core
 import flutter_tts
 import path_provider_foundation
 import share_plus
@@ -13,6 +16,9 @@ import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
