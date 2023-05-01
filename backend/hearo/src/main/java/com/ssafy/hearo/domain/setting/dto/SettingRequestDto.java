f39e6882d2e6dad7847e48a90ec821bcc095ebcc
nayeonkinn <nayeonkinn@gmail.com>
2023-05-01 15:21:16 +0900

    [S08P31A603-317] Update: 프로필 api 예외 처리

--- backend/hearo/src/main/java/com/ssafy/hearo/domain/setting/dto/SettingRequestDto.java
+++ backend/hearo/src/main/java/com/ssafy/hearo/domain/setting/dto/SettingRequestDto.java
@@ -2,14 +2,15 @@ package com.ssafy.hearo.domain.setting.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 
 public class SettingRequestDto {
 
     @Getter
     public static class ModifySettingRequestDto {
 
-        private long settingSeq;
-        private long userSeq;
         private byte fontSize;
         private byte voiceSetting;
     }
@@ -17,6 +18,7 @@ public class SettingRequestDto {
     @Getter
     public static class FrequentRequestDto {
 
+        @NotBlank
         private String sentence;
     }
 }
