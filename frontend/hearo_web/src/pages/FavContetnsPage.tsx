82258f4840ff968df81e20a54fc4132a49c4e7f0
gs20wow <gs20wow@gmail.com>
2023-05-01 17:18:20 +0900

    [S08P31A603-290] Feat : 자주쓰는 말 페이지 전체 수정
    상세설명
    -

--- frontend/hearo_web/src/pages/FavContetnsPage.tsx
+++ frontend/hearo_web/src/pages/FavContetnsPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { ArrowLeftIcon, XMarkIcon  } from "@heroicons/react/24/solid";
 import { useNavigate } from "react-router-dom";
 import { Modal } from "@/components";
@@ -15,6 +15,8 @@ function FavContentsPage () {
   const [id, setId] = useState<number>(0);
   const [showModal, setShowModal] = useState<boolean>(false);
   const [modalType, setModalType] = useState<'add' | 'delete'>('add');
+  // const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   const [contents, setContents] = useState<MessageType[]>([
     { id: 1, content: '안녕하세요 저는 청각장애인입니다.', speaker: 'user' },
     { id: 2, content: '뭐 먹었어?', speaker: 'user' },
@@ -23,40 +25,61 @@ function FavContentsPage () {
     { id: 5, content: '스시 먹었어?', speaker: 'user' },
   ]);
 
+  // 상단bar 스타일
   const mypagebarBackground = "z-10 bg-white drop-shadow";
   const navigate = useNavigate();
   const backClick = () => {
     navigate('/mypage');
   }
 
+  // 추가 버튼
   const onAddButtonClick = () => {
     setModalType('add');
     setShowModal(true);
-    setId(0); // reset id state
   };
 
+  // 겹치지 않게 id를 기억한다
+  const [idToDelete, setIdToDelete] = useState(0);
   const onDeleteButtonClick = (id: number) => {
     setModalType('delete');
+    setIdToDelete(id);
     setId(id);
     setShowModal(true);
-    console.log('삭제 안되나?')
   };
-  
-  const onModalSave = (content: any) => {
+
+  // 중복되는 내용이면 입력을 막는 함수
+  const isDuplicateContent = (contentToCheck : string) => {
+    return contents.some((content) => content.content === contentToCheck);
+  }
+
+  // 내용이 입력되었는지 안되었는지 확인하는 함수
+  const [inputValue, setInputValue] = useState('');
+  // 길어서 주의가 필요함 
+  const onModalSave = () => {
     if (modalType === 'add') {
+      if (inputValue.trim() === ''){
+        setInputValue('');
+        alert('내용을 입력해주세요!')
+        return;
+      }
+      if (isDuplicateContent(inputValue)) {
+        setInputValue('');
+        alert('이미 추가된 내용입니다!');
+        return;
+      }
       const newId = contents.length + 1;
-      setContents([...contents, { id: newId, content: content, speaker: 'user' }]);
+      setContents([...contents, { id: newId, content: inputValue , speaker: 'user' }]);
     } else if (modalType === 'delete') {
-      const newContents = contents.filter(c => c.id !== id);
+      const newContents = contents.filter(c => c.id !== idToDelete);
       setContents(newContents);
     }
+    setInputValue('');
     setShowModal(false);
   };
-  
+
   const ModalOff = () => {
     setShowModal(false);
   }
-
 
   return (
     <div>
@@ -98,7 +121,8 @@ function FavContentsPage () {
           {modalType === 'add' && (
            <div>
             <div className="mb-2 text-xl font-semibold">자주 쓰는 말 추가</div>
-            <input type="text" className="w-full border border-gray-200 p-2 rounded" autoFocus />
+            <input type="text" className="w-full border border-gray-200 p-2 rounded" autoFocus 
+            value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
             <div className="flex flex-row text-2xl justify-center m-1 mt-3 font-bold">
             <button onClick={ModalOff} className="mt-2 pl-4 pr-4 border-gray-950">
                취소 
@@ -117,7 +141,7 @@ function FavContentsPage () {
                 <button onClick={ModalOff} className="mt-2 pl-4 pr-4 border-gray-950">
                   취소 
                 </button>
-                <button onClick={() => onDeleteButtonClick(id)} className="mt-2 bg-red-1 hover:bg-red-main text-white py-2 px-4 rounded"> 
+                <button onClick={onModalSave} className="mt-2 bg-red-1 hover:bg-red-main text-white py-2 px-4 rounded"> 
                   삭제
                 </button>
               </div>
