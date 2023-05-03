68304ae96489ef792849f46634a39a9d83583eea
nayeonkinn <nayeonkinn@gmail.com>
2023-05-03 17:55:53 +0900

    [S08P31A603-336] Feat: 배포 관련 파일 추가

---
+++ backend/hearo_fastapi/human_jenkins.sh
@@ -0,0 +1,17 @@
+echo "1. stop container"
+docker stop hearo-fastapi
+
+echo "2. remove container"
+docker rm hearo-fastapi
+
+echo "3. remove image"
+docker rmi fastapi
+
+echo "4. re-build image"
+docker build -t fastapi .
+
+echo "5. re-run container"
+docker run -d -p 80:80 --shm-size 2000000000 --name hearo-fastapi fastapi
+
+echo "6. check! i'm the korean topclass hiphop mobeom nobless"
+docker ps -a
