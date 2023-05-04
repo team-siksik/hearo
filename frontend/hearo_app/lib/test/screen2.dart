18f647561c6bd75bcd13699b0277f94e35f1517c
cjfal2 <cjfal2@kakao.com>
2023-05-04 14:57:17 +0900

    [S08P31A603-000] Feat: 녹음 트라이
    상세설명
    - 녹음 도전

--- frontend/hearo_app/lib/test/screen2.dart
+++ frontend/hearo_app/lib/test/screen2.dart
@@ -9,81 +9,144 @@ class Screen2 extends StatefulWidget {
 }
 
 class _Screen2State extends State<Screen2> {
-  FlutterBluePlus blue = FlutterBluePlus.instance;
-  List result = [];
-  bool check = false;
-  String viewTxt = "";
+  FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
+  List<ScanResult> scanResultList = [];
+  bool _isScanning = false;
+  final String targetDeviceName = 'Galaxy Watch5 (2RAD)';
+
   @override
   initState() {
     super.initState();
-    blue.scanResults.listen((results) {
-      print("검색중 ...");
-      print("results : $results");
-      if (results.isNotEmpty) {
-        setState(() {
-          result = results;
-        });
-      }
-    });
-    blue.connectedDevices.asStream().listen((List<BluetoothDevice> devices) {
-      for (BluetoothDevice device in devices) {
-        print("device: $device");
-      }
+    // 블루투스 초기화
+    initBle();
+  }
+
+  void initBle() {
+    // BLE 스캔 상태 얻기 위한 리스너
+    flutterBlue.isScanning.listen((isScanning) {
+      _isScanning = isScanning;
+      setState(() {});
     });
   }
 
-  Future blueBtn() async {
-    setState(() {
-      check = true;
-      viewTxt = "검색중...";
-    });
-    var bl = await blue
-        .startScan(
-            scanMode: ScanMode.balanced,
-            allowDuplicates: true,
-            timeout: Duration(seconds: 12))
-        .timeout(Duration(seconds: 12), onTimeout: () async {
-      await blue.stopScan();
-      setState(() {
-        check = false;
-        viewTxt = "ERR";
-      });
-    });
-    print("startScan : $bl");
-
-    await Future.delayed(Duration(seconds: 13), () async {
-      await blue.stopScan();
-      setState(() {
-        check = false;
-        if (result.isEmpty) {
-          viewTxt = "대기중...";
+  /*
+  스캔 시작/정지 함수
+  */
+  scan() async {
+    if (!_isScanning) {
+      // 스캔 중이 아니라면
+      // 기존에 스캔된 리스트 삭제
+      scanResultList.clear();
+      // 스캔 시작, 제한 시간 4초
+      flutterBlue.startScan(timeout: Duration(seconds: 4));
+      // 스캔 결과 리스너
+      flutterBlue.scanResults.listen((results) {
+        // 결과 값을 루프로 돌림
+        for (var element in results) {
+          //찾는 장치명인지 확인
+          if (element.device.name == targetDeviceName) {
+            // 장치의 ID를 비교해 이미 등록된 장치인지 확인
+            if (scanResultList
+                    .indexWhere((e) => e.device.id == element.device.id) <
+                0) {
+              // 찾는 장치명이고 scanResultList에 등록된적이 없는 장치라면 리스트에 추가
+              scanResultList.add(element);
+            }
+          }
+          // UI 갱신
+          setState(() {});
         }
       });
-    });
+    } else {
+      // 스캔 중이라면 스캔 정지
+      flutterBlue.stopScan();
+    }
+  }
+
+  /*
+   여기서부터는 장치별 출력용 함수들
+  */
+  /*  장치의 신호값 위젯  */
+  Widget deviceSignal(ScanResult r) {
+    return Text(r.rssi.toString());
+  }
+
+  /* 장치의 MAC 주소 위젯  */
+  Widget deviceMacAddress(ScanResult r) {
+    return Text(r.device.id.id);
+  }
+
+  /* 장치의 명 위젯  */
+  Widget deviceName(ScanResult r) {
+    String name = '';
+
+    if (r.device.name.isNotEmpty) {
+      // device.name에 값이 있다면
+      name = r.device.name;
+    } else if (r.advertisementData.localName.isNotEmpty) {
+      // advertisementData.localName에 값이 있다면
+      name = r.advertisementData.localName;
+    } else {
+      // 둘다 없다면 이름 알 수 없음...
+      name = 'N/A';
+    }
+    return Text(name);
+  }
+
+  /* BLE 아이콘 위젯 */
+  Widget leading(ScanResult r) {
+    return CircleAvatar(
+      backgroundColor: Colors.cyan,
+      child: Icon(
+        Icons.bluetooth,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  /* 장치 아이템을 탭 했을때 호출 되는 함수 */
+  void onTap(ScanResult r) {
+    // 단순히 이름만 출력
+    print(r.device.name);
+  }
+
+  /* 장치 아이템 위젯 */
+  Widget listItem(ScanResult r) {
+    return ListTile(
+      onTap: () => onTap(r),
+      leading: leading(r),
+      title: deviceName(r),
+      subtitle: deviceMacAddress(r),
+      trailing: deviceSignal(r),
+    );
   }
 
   /* UI */
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("블루투스"),
+      appBar: AppBar(
+        title: Text("블루투스"),
+      ),
+      body: Center(
+        /* 장치 리스트 출력 */
+        child: ListView.separated(
+          itemCount: scanResultList.length,
+          itemBuilder: (context, index) {
+            final res = scanResultList[index];
+            return listItem(res);
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return Divider();
+          },
         ),
-        body: Center(
-            /* 장치 리스트 출력 */
-            child: Column(
-          children: [
-            TextButton(
-                onPressed: () {
-                  blueBtn();
-                },
-                child: Text("Blue")),
-            Container(
-                height: 500,
-                padding: EdgeInsets.all(10),
-                color: check ? Colors.blue : Colors.red,
-                child: SingleChildScrollView(child: Text(result.toString())))
-          ],
-        )));
+      ),
+      /* 장치 검색 or 검색 중지  */
+      floatingActionButton: FloatingActionButton(
+        onPressed: scan,
+        // 스캔 중이라면 stop 아이콘을, 정지상태라면 search 아이콘으로 표시
+        child: Icon(_isScanning ? Icons.stop : Icons.search),
+      ),
+    );
   }
 }
