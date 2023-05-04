463f5b16ba3d716d89684c37587b06aad647d669
cjfal2 <cjfal2@kakao.com>
2023-05-04 12:32:29 +0900

    [S08P31A603-312] Feat: 블루투스 찾기 성공
    상세설명
    - 찾기만 성공
    
    
    Former-commit-id: e7e64804e69c261f88bd1b1bf4374369163e2c16

--- frontend/hearo_app/lib/test/screen2.dart
+++ frontend/hearo_app/lib/test/screen2.dart
@@ -1,39 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
-import 'string_list_controller.dart';
+class Screen2 extends StatefulWidget {
+  const Screen2({super.key});
 
-class Screen2 extends StatelessWidget {
-  final controller = Get.put(StringListController());
+  @override
+  State<Screen2> createState() => _Screen2State();
+}
 
-  Screen2({super.key});
+class _Screen2State extends State<Screen2> {
+  FlutterBluePlus blue = FlutterBluePlus.instance;
+  List result = [];
+  bool check = false;
+  String viewTxt = "";
+  @override
+  initState() {
+    super.initState();
+    blue.scanResults.listen((results) {
+      print("검색중 ...");
+      print("results : $results");
+      if (results.isNotEmpty) {
+        setState(() {
+          result = results;
+        });
+      }
+    });
+    blue.connectedDevices.asStream().listen((List<BluetoothDevice> devices) {
+      for (BluetoothDevice device in devices) {
+        print("device: $device");
+      }
+    });
+  }
 
+  Future blueBtn() async {
+    setState(() {
+      check = true;
+      viewTxt = "검색중...";
+    });
+    var bl = await blue
+        .startScan(
+            scanMode: ScanMode.balanced,
+            allowDuplicates: true,
+            timeout: Duration(seconds: 12))
+        .timeout(Duration(seconds: 12), onTimeout: () async {
+      await blue.stopScan();
+      setState(() {
+        check = false;
+        viewTxt = "ERR";
+      });
+    });
+    print("startScan : $bl");
+
+    await Future.delayed(Duration(seconds: 13), () async {
+      await blue.stopScan();
+      setState(() {
+        check = false;
+        if (result.isEmpty) {
+          viewTxt = "대기중...";
+        }
+      });
+    });
+  }
+
+  /* UI */
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Screen 2'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Obx(() => ListView.builder(
-                  itemCount: controller.stringList.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(controller.stringList[index]),
-                    );
-                  },
-                )),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              controller.clearList();
-            },
-            child: Text('Clear'),
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("블루투스"),
+        ),
+        body: Center(
+            /* 장치 리스트 출력 */
+            child: Column(
+          children: [
+            TextButton(
+                onPressed: () {
+                  blueBtn();
+                },
+                child: Text("Blue")),
+            Container(
+                height: 500,
+                padding: EdgeInsets.all(10),
+                color: check ? Colors.blue : Colors.red,
+                child: SingleChildScrollView(child: Text(result.toString())))
+          ],
+        )));
   }
 }
