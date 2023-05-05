ac6c85b2df835a2996094793eb95cd237b3f2872
nayeonkinn <nayeonkinn@gmail.com>
2023-05-05 17:58:47 +0000

    [S08P31A603-353] Feat: time to string util 추가

--- backend/hearo_fastapi/sl_recognition/create_dataset.py
+++ backend/hearo_fastapi/sl_recognition/create_dataset.py
@@ -4,14 +4,17 @@ import numpy as np
 import time
 import os
 
-with open('words.txt', 'r') as f:
+from util import time_to_string
+
+
+with open("words.txt", "r") as f:
     words = f.readlines()
     words = [word.strip() for word in words]
 seq_length = 30
 secs_for_action = 30
 
-created_time = int(time.time())
-os.makedirs('dataset', exist_ok=True)
+created_time = time_to_string(time.time())
+os.makedirs("dataset", exist_ok=True)
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -19,10 +22,9 @@ mp_hands = mp.solutions.hands
 
 cap = cv2.VideoCapture(0)
 
-with mp_hands.Hands(model_complexity=0,
-                    min_detection_confidence=0.5,
-                    min_tracking_confidence=0.5) as hands:
-
+with mp_hands.Hands(
+    model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5
+) as hands:
     while cap.isOpened():
         for idx, word in enumerate(words):
             data = []
@@ -35,9 +37,16 @@ with mp_hands.Hands(model_complexity=0,
 
             image = cv2.flip(image, 1)
 
-            cv2.putText(image, f'Waiting for collecting {word.upper()} action...', org=(
-                10, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
-            cv2.imshow('image', image)
+            cv2.putText(
+                image,
+                f"Waiting for collecting {word.upper()} action...",
+                org=(10, 30),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=1,
+                color=(255, 255, 255),
+                thickness=2,
+            )
+            cv2.imshow("image", image)
             cv2.waitKey(3000)
 
             start_time = time.time()
@@ -57,42 +66,106 @@ with mp_hands.Hands(model_complexity=0,
                 left_joint, right_joint = np.zeros((21, 4)), np.zeros((21, 4))
                 left_angle, right_angle = np.zeros((15,)), np.zeros((15,))
 
-                for landmarks, hand in zip(results.multi_hand_landmarks, results.multi_handedness):
+                for landmarks, hand in zip(
+                    results.multi_hand_landmarks, results.multi_handedness
+                ):
                     hand = hand.classification[0].label.lower()
 
                     for j, lm in enumerate(landmarks.landmark):
-                        globals()['{}_joint'.format(hand)][j] = [
-                            lm.x, lm.y, lm.z, lm.visibility]
+                        globals()["{}_joint".format(hand)][j] = [
+                            lm.x,
+                            lm.y,
+                            lm.z,
+                            lm.visibility,
+                        ]
 
                     # Compute angles between joints
-                    v1 = globals()['{}_joint'.format(hand)][[0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11,
-                                                             0, 13, 14, 15, 0, 17, 18, 19], :3]  # Parent joint
-                    v2 = globals()['{}_joint'.format(hand)][[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-                                                             13, 14, 15, 16, 17, 18, 19, 20], :3]  # Child joint
+                    v1 = globals()["{}_joint".format(hand)][
+                        [
+                            0,
+                            1,
+                            2,
+                            3,
+                            0,
+                            5,
+                            6,
+                            7,
+                            0,
+                            9,
+                            10,
+                            11,
+                            0,
+                            13,
+                            14,
+                            15,
+                            0,
+                            17,
+                            18,
+                            19,
+                        ],
+                        :3,
+                    ]  # Parent joint
+                    v2 = globals()["{}_joint".format(hand)][
+                        [
+                            1,
+                            2,
+                            3,
+                            4,
+                            5,
+                            6,
+                            7,
+                            8,
+                            9,
+                            10,
+                            11,
+                            12,
+                            13,
+                            14,
+                            15,
+                            16,
+                            17,
+                            18,
+                            19,
+                            20,
+                        ],
+                        :3,
+                    ]  # Child joint
                     v = v2 - v1  # [20, 3]
 
                     # Normalize v
                     v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
 
                     # Get angle using arcos of dot product
-                    angle = np.arccos(np.einsum('nt,nt->n',
-                                                v[[0, 1, 2, 4, 5, 6, 8, 9, 10,
-                                                    12, 13, 14, 16, 17, 18], :],
-                                                v[[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19], :]))  # (15,)
+                    angle = np.arccos(
+                        np.einsum(
+                            "nt,nt->n",
+                            v[[0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18], :],
+                            v[[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19], :],
+                        )
+                    )  # (15,)
 
                     # Convert radian to degree
-                    globals()['{}_angle'.format(hand)] = np.degrees(angle)
-                    globals()['{}_angle'.format(hand)] = np.array(
-                        angle, dtype=np.float32)
+                    globals()["{}_angle".format(hand)] = np.degrees(angle)
+                    globals()["{}_angle".format(hand)] = np.array(
+                        angle, dtype=np.float32
+                    )
 
-                    mp_drawing.draw_landmarks(image,
-                                              landmarks,
-                                              mp_hands.HAND_CONNECTIONS,
-                                              mp_drawing_styles.get_default_hand_landmarks_style(),
-                                              mp_drawing_styles.get_default_hand_connections_style())
+                    mp_drawing.draw_landmarks(
+                        image,
+                        landmarks,
+                        mp_hands.HAND_CONNECTIONS,
+                        mp_drawing_styles.get_default_hand_landmarks_style(),
+                        mp_drawing_styles.get_default_hand_connections_style(),
+                    )
 
                 d = np.concatenate(
-                    [left_joint.flatten(), left_angle, right_joint.flatten(), right_angle])
+                    [
+                        left_joint.flatten(),
+                        left_angle,
+                        right_joint.flatten(),
+                        right_angle,
+                    ]
+                )
                 d = np.append(d, idx)
                 data.append(d)  # (199,)
 
@@ -102,18 +175,18 @@ with mp_hands.Hands(model_complexity=0,
 
             data = np.array(data)
             print(word, data.shape)
-            np.save(os.path.join(
-                'dataset', f'raw_{word}_{created_time}'), data)
+            np.save(os.path.join("dataset", f"raw_{word}_{created_time}"), data)
 
             # Create sequence data
             full_seq_data = []
             for seq in range(len(data) - seq_length):
-                full_seq_data.append(data[seq:seq + seq_length])
+                full_seq_data.append(data[seq : seq + seq_length])
 
             full_seq_data = np.array(full_seq_data)
             print(word, full_seq_data.shape)
-            np.save(os.path.join(
-                'dataset', f'seq_{word}_{created_time}'), full_seq_data)
+            np.save(
+                os.path.join("dataset", f"seq_{word}_{created_time}"), full_seq_data
+            )
         break
 
 cap.release()
