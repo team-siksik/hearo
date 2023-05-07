55bf05b0bdc6326c56802dde5afd5d2ebda35ebf
Young312886 <hongym0497@gmail.com>
2023-05-08 01:47:44 +0900

    Fix: 소켓 활용 코드 부분 수정

--- backend/hearo_ai/ai_codes/try_socket.py
+++ backend/hearo_ai/ai_codes/try_socket.py
@@ -4,21 +4,18 @@ import sys
 import wave
 import base64
 import time
-import librosa
-from diart.utils import encode_audio
 
 async def run_test(uri):
     async with websockets.connect(uri) as websocket:
-        wf, rate = librosa.load(sys.argv[1], sr=16000)
-        for i in range(0, len(wf), 8000):
-            encoded_string = encode_audio(wf[i:i+8000])
-            print(str(encoded_string))
+
+        wf = wave.open(sys.argv[1], "rb")
+        while True:
+            encoded_string = base64.encodebytes(wf.readframes(1024))
+            print(encoded_string)
             if len(encoded_string) == 0:
                  break
             
             await websocket.send(str(encoded_string))
             time.sleep(0.1)
-        while True:
-            print(await websocket.recv())
 
-asyncio.run(run_test('ws://localhost:8090'))
+asyncio.run(run_test('ws://localhost:7007'))
