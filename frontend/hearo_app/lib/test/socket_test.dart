c1ff1f51708b3fbf8daf99b03c95a5fb2caf6365
cjfal2 <cjfal2@kakao.com>
2023-05-09 00:14:55 +0900

    [S08P31A603-000] Feat: 소켓 및 stt 푸시
    상세설명
    - 소켓 및 stt 푸시 연습중

--- frontend/hearo_app/lib/test/socket_test.dart
+++ frontend/hearo_app/lib/test/socket_test.dart
@@ -10,31 +10,78 @@ class SocketTest extends StatefulWidget {
 
 class _SocketTestState extends State<SocketTest> {
   late so_io.Socket socket;
-
+  List messages = [];
   @override
   void initState() {
     super.initState();
 
     // Connect to the server
     socket = so_io.io('http://k8a6031.p.ssafy.io:80/ws', <String, dynamic>{
-      'transports': ['polling'],
+      'transports': ['websocket'],
       'autoConnect': false,
       'path': '/ws/socket.io',
     });
-    socket.connect();
-    socket.onConnect((_) {
-      print('connect');
-      socket.emit('msg', 'test');
-    });
-    socket.on('event', (data) => print(data));
-    socket.onDisconnect((_) => print('disconnect'));
-    socket.on('fromServer', (_) => print(_));
+    connect();
+    // socket.onConnect((_) {
+    //   print('connect');
+    //   socket.emit('msg', 'test');
+    // });
+    // socket.on('event', (data) => print(data));
+    // socket.onDisconnect((_) => print('disconnect'));
+    // socket.on('fromServer', (_) => print(_));
   }
 
   @override
   void dispose() {
-    socket.disconnect();
+    disconnect();
     super.dispose();
+  }
+
+  void connect() {
+    socket.connect();
+  }
+
+  void disconnect() {
+    socket.disconnect();
+  }
+
+  void enterRoom(String roomId) {
+    socket.emit('enter_room', {'room_id': roomId});
+    print("방 입장");
+  }
+
+  void leaveRoom(String roomId) {
+    socket.emit('leave_room', {'room_id': roomId});
+  }
+
+  void sendMessageToRoom(String roomId, String message) {
+    socket
+        .emit('send_message_to_room', {'room_id': roomId, 'message': message});
+    print(message);
+    print("메세지보냄");
+  }
+
+  void onConnect(Function(dynamic) callback) {
+    socket.on('connect', (_) {
+      print('connected');
+      print(socket.connected); // 연결 여부 확인
+      callback(_);
+    });
+  }
+
+  void onDisconnect(Function(dynamic) callback) {
+    socket.on('disconnect', callback);
+  }
+
+  void onInfo(Function(dynamic) callback) {
+    socket.on('info', callback);
+  }
+
+  void onMessage(Function(dynamic) callback) {
+    socket.on('message', callback);
+    setState(() {
+      messages.add(callback);
+    });
   }
 
   @override
@@ -44,7 +91,47 @@ class _SocketTestState extends State<SocketTest> {
         title: Text('SocketIO Flutter Example'),
       ),
       body: Center(
-        child: Text('SocketIO Flutter Example'),
+        child: Column(
+          children: [
+            TextButton(
+                onPressed: () {
+                  connect();
+                },
+                child: Text("소켓연결")),
+            TextButton(
+                onPressed: () {
+                  enterRoom('1111');
+                },
+                child: Text("소켓방드가기")),
+            TextButton(
+                onPressed: () {
+                  onConnect((p0) => print(p0));
+                },
+                child: Text("소켓확인")),
+            TextButton(
+                onPressed: () {
+                  sendMessageToRoom("1111", "어뜨케된겨");
+                },
+                child: Text("메시지전송")),
+            TextButton(
+                onPressed: () {
+                  onMessage(
+                    (p0) {},
+                  );
+                },
+                child: Text("메시지확인")),
+            Expanded(
+              child: ListView.builder(
+                itemCount: messages.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(messages[index]),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- frontend/hearo_app/lib/test/speech_screen.dart
+++ frontend/hearo_app/lib/test/speech_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_to_text.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class SpeechScreen extends StatefulWidget {
   const SpeechScreen({super.key});
@@ -9,49 +9,49 @@ class SpeechScreen extends StatefulWidget {
 }
 
 class _SpeechScreenState extends State<SpeechScreen> {
-  final SpeechToText _speech = SpeechToText();
-  String saying = '';
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = '';
 
   @override
   void initState() {
     super.initState();
-    _initSpeechRecognition();
+    _speech = stt.SpeechToText();
   }
 
-  Future<void> _initSpeechRecognition() async {
-    bool hasSpeech = await _speech.initialize(
-      onError: (val) => print('Error: $val'),
-      onStatus: (val) => print('Status: $val'),
-    );
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (status) {
+          if (status == 'done') {
+            _isListening = false;
+          }
+          setState(() {});
+        },
+        onError: (error) {
+          print('Error: $error');
+        },
+      );
 
-    if (hasSpeech) {
-      _startListening();
-    }
-  }
-
-  bool flag = false;
-  void _startListening() async {
-    setState(() {
-      flag = false;
-    });
-    await _speech.listen(
-      onResult: (val) => setState(() {
-        saying = val.recognizedWords;
-      }),
-      listenFor: Duration(seconds: 10), // 듣기 지속 시간을 10초로 설정합니다.
-      pauseFor: Duration(milliseconds: 0),
-      partialResults: true,
-    );
-    bool hasSpeech = await _speech.initialize(
-      onError: (val) => print('Error: $val'),
-      onStatus: (val) {
+      if (available) {
         setState(() {
-          flag = true;
+          _isListening = true;
         });
-      },
-    );
-    if (true) {
-      _startListening();
+
+        _speech.listen(
+          onResult: (result) {
+            setState(() {
+              _text = result.recognizedWords;
+            });
+          },
+        );
+      }
+    } else {
+      setState(() {
+        _isListening = false;
+      });
+
+      _speech.stop();
     }
   }
 
@@ -59,13 +59,18 @@ class _SpeechScreenState extends State<SpeechScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Speech To Text Demo'),
+        title: Text('STT Example'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(saying),
+          children: [
+            Text(_text),
+            SizedBox(height: 20),
+            FloatingActionButton(
+              onPressed: _listen,
+              child: Icon(_isListening ? Icons.stop : Icons.mic),
+            ),
           ],
         ),
       ),
