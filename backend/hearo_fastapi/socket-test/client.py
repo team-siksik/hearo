1723c429d35feda45a8160d0f3f5ec1811bcf86a
nayeonkinn <nayeonkinn@gmail.com>
2023-05-08 14:53:53 +0900

    [S08P31A603-409] Update: 소켓 통신 테스트

--- backend/hearo_fastapi/socket-test/client.py
+++ backend/hearo_fastapi/socket-test/client.py
@@ -3,17 +3,20 @@ import socketio
 sio = socketio.Client()
 
 
-@sio.on("join")
-def join(data):
-    print("Joined!")
-    print(data)
+@sio.on("info")
+def info(data):
+    print(f"info: {data}")
 
 
-@sio.on("test")
+@sio.on("message")
 def test(data):
-    print("Message Received!")
-    print(data)
+    print(f"message: {data}")
 
 
 sio.connect("http://127.0.0.1:8000/ws", socketio_path="/ws/socket.io", wait_timeout=5)
 sio.wait()
+
+sio.emit("enter_room", "hi")
+sio.emit("leave_room", "hi")
+
+sio.emit("disconnect")
--- backend/hearo_fastapi/socket-test/server.py
+++ backend/hearo_fastapi/socket-test/server.py
@@ -1,23 +1,67 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 from fastapi_socketio import SocketManager
 
+
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 socket_manager = SocketManager(app=app)
 
 
-@app.get("/socket.io")
-async def socket():
-    print("socket!")
+@app.get("/test", response_class=HTMLResponse)
+async def test():
+    f = open("example.html", encoding="UTF-8")
+    r = f.read()
+    return HTMLResponse(content=r)
 
 
-@socket_manager.on("join")
-async def handle_join(sid, *args, **kwargs):
-    await socket_manager.emit("join", "User joined")
+@socket_manager.on("connect")
+async def connect(sid, environ):
+    await socket_manager.emit("info", f"{sid} connected")
+    print(f"{sid} connected")
 
 
-@socket_manager.on("test")
-async def test(sid, *args, **kwargs):
-    await socket_manager.emit("test", "test")
+@socket_manager.on("disconnect")
+async def disconnect(sid):
+    await socket_manager.emit("info", f"{sid} disconnected")
+    print(f"{sid} disconnected")
+
+
+@socket_manager.on("enter_room")
+async def enter_room(sid, data):
+    room_id = data["room_id"]
+    await socket_manager.enter_room(sid, room_id)
+    await socket_manager.emit("info", f"{sid} entered room '{room_id}'")
+    print(f"{sid} entered room '{room_id}'")
+
+
+@socket_manager.on("leave_room")
+async def leave_room(sid, data):
+    room_id = data["room_id"]
+    await socket_manager.leave_room(sid, room_id)
+    await socket_manager.emit("info", f"{sid} left room '{room_id}'")
+    print(f"{sid} left room '{room_id}'")
+
+
+@socket_manager.on("send_message_to_room")
+async def send_message_to_room(sid, data):
+    room_id = data["room_id"]
+    message = data["message"]
+    await socket_manager.emit("message", message, room_id, skip_sid=sid)
+    await socket_manager.emit(
+        "info", f"{sid} sent message '{message}' to room '{room_id}'"
+    )
+    print(f"{sid} sent message '{message}' to room '{room_id}'")
 
 
 if __name__ == "__main__":
