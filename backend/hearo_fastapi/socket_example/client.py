0db1ba1f9658a83168f2e798b40584dfcfce6fcc
nayeonkinn <nayeonkinn@gmail.com>
2023-05-09 22:07:51 +0900

    [S08P31A603-440] Update: router 분리
    Former-commit-id: 8dccfc37a36578dbd1579763b8408615a5f19960

--- backend/hearo_fastapi/socket_example/client.py
+++ backend/hearo_fastapi/socket_example/client.py
@@ -13,10 +13,10 @@ def test(data):
     print(f"message: {data}")
 
 
-# sio.connect("http://localhost:8000/ws", socketio_path="/ws/socket.io", wait_timeout=5)
-sio.connect(
-    "http://k8a6031.p.ssafy.io:80/ws", socketio_path="/ws/socket.io", wait_timeout=5
-)
+sio.connect("http://localhost:8000/ws", socketio_path="/ws/socket.io", wait_timeout=5)
+# sio.connect(
+#     "http://k8a6031.p.ssafy.io:80/ws", socketio_path="/ws/socket.io", wait_timeout=5
+# )
 # sio.wait()
 
 sio.emit("enter_room", {"room_id": "hi"})
