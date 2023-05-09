b7c1166336b05a2f096f0b0ce531f699299687dc
gs20wow <gs20wow@gmail.com>
2023-05-09 17:56:51 +0900

    [S08P31A603-362] Feat : 전체 스타일 수정
    상세설명
    -

--- frontend/hearo_web/src/components/Record/RecordsItem.tsx
+++ frontend/hearo_web/src/components/Record/RecordsItem.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import { TrashIcon } from "@heroicons/react/24/solid";
+import { NewspaperIcon } from "@heroicons/react/24/outline";
 import { useNavigate } from "react-router-dom";
 
 interface RecordsItemProps {
@@ -9,22 +10,24 @@ interface RecordsItemProps {
   description: string;
   onDelete: () => void;
 }
-const [openRemoveRecordModal, setOpenRemoveRecordModal] = useState<boolean>(false);
-
-const onDeleteModal = () => {
-  setOpenRemoveRecordModal(true);
-}
 
 function RecordsItem({ id, title, date, description, onDelete}:RecordsItemProps) {
   return (
         <div className="mx-10 mt-4 p-4 rounded-2xl font-semibold text-black hover:cursor-pointer hover:bg-blue-50 transition-all duration-300 ease-out shadow-lg">
           <div className="flex flex-row items-stretch justify-between">
-            <div>
-              <div className="font-bold mb-2">{title}</div>
-              <div className="text-sm">{date}</div>
-              <div className="mt-2">{description}</div>
+            <div className="flex items-center">
+                <div className="self-center mr-4 font-semibold text-blue-main rounded-full bg-blue-100 w-12 h-12 flex justify-center items-center">
+                  <div className="w-6 h-6">
+                    <NewspaperIcon />
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold mb-2">{title}</div>
+                  <div className="my-2 text-blue-main">{description}</div>
+                  <div className="text-sm">{date}</div>
+                </div>
             </div>
-            <div onClick={onDeleteModal} className="self-center mr-4 font-semibold text-red-main rounded-full bg-red-50 w-12 h-12 hover:bg-red-300 hover:shadow-sm transition-all duration-200 ease-out flex justify-center items-center">
+            <div className="self-center mr-4 font-semibold text-red-main rounded-full bg-red-50 w-12 h-12 hover:bg-red-300 hover:shadow-sm transition-all duration-200 ease-out flex justify-center items-center">
               <div className="w-6 h-6">
                 <TrashIcon />
               </div>
@@ -34,3 +37,4 @@ function RecordsItem({ id, title, date, description, onDelete}:RecordsItemProps)
   );
 }
 export default RecordsItem;
+
