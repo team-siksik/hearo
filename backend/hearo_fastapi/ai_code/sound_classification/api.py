332f6faf40678b7065e38ff8d8b5d68b1c1abbf4
nayeonkinn <nayeonkinn@gmail.com>
2023-05-10 12:13:11 +0000

    Fix: requirements.txt 오류 라이브러리 및 소리 인식 주석 처리

--- backend/hearo_fastapi/ai_code/sound_classification/api.py
+++ backend/hearo_fastapi/ai_code/sound_classification/api.py
@@ -1,31 +1,31 @@
-import requests
-import api_token
-from typing import Dict
+# import requests
+# import api_token
+# from typing import Dict
 
-API_TOKEN = api_token.huggingface_api_token
-API_URL = "https://api-inference.huggingface.co/models/speechbrain/urbansound8k_ecapa"
-headers = {"Authorization": f"Bearer {API_TOKEN}"}
+# API_TOKEN = api_token.huggingface_api_token
+# API_URL = "https://api-inference.huggingface.co/models/speechbrain/urbansound8k_ecapa"
+# headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-ko_dict = {
-    "dog_bark": "개",
-    "children_playing": "어린이",
-    "air_conditioner": "에어컨",
-    "street_music": "음악",
-    "gun_shot": "총",
-    "siren": "사이렌",
-    "engine_idling": "엔진",
-    "jackhammer": "착암기",
-    "drilling": "드릴",
-    "car_horn": "자동차 경적"
-}
+# ko_dict = {
+#     "dog_bark": "개",
+#     "children_playing": "어린이",
+#     "air_conditioner": "에어컨",
+#     "street_music": "음악",
+#     "gun_shot": "총",
+#     "siren": "사이렌",
+#     "engine_idling": "엔진",
+#     "jackhammer": "착암기",
+#     "drilling": "드릴",
+#     "car_horn": "자동차 경적"
+# }
 
-def query(audio_data: bytes) -> Dict:
-    response = requests.post(API_URL, headers=headers, data=audio_data)
-    return response.json()
+# def query(audio_data: bytes) -> Dict:
+#     response = requests.post(API_URL, headers=headers, data=audio_data)
+#     return response.json()
 
-def query_with_memory(audio_data: bytes):
-    output = query(audio_data)
-    for result in output:
-        if result['label'] in ko_dict:
-            return ko_dict[result['label']]
-    return None
+# def query_with_memory(audio_data: bytes):
+#     output = query(audio_data)
+#     for result in output:
+#         if result['label'] in ko_dict:
+#             return ko_dict[result['label']]
+#     return None
