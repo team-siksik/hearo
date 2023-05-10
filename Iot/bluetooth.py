438934e48bbfd2aca156a1b112682e73b3a0d9a9
Young312886 <hongym0497@gmail.com>
2023-05-10 17:50:44 +0900

    Feat: bluetooth 샘플 코드

--- Iot/bluetooth.py
+++ Iot/bluetooth.py
@@ -1,19 +1,8 @@
-from machine import Pin, UART
-uart = UART(0, 9600)
-
-print('start pico bluetooth test')
-cmd = ''
-CommandStarted = False
-led = Pin(25, Pin.OUT)
+import board
+import busio
+bluetooth = busio.UART(board.GP0, board.GP1, baudrate=9600)
 while True:
-    if uart.any():
-        command = uart.readline()
-        print(command)   # uncomment this line to see the recieved data
-        if command==b'1':
-            led.high()
-            print("ON")
-        elif command==b'0':
-            led.low()
-            print("OFF")
-
-                
+    response = bluetooth.readline()
+    if response is not None:
+        print(response)
+        print(response.decode())
