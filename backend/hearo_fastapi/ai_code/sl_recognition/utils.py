fbb80000d3a76575f5682a29f9615db3bf9aa57d
nayeonkinn <nayeonkinn@gmail.com>
2023-05-11 02:52:26 +0900

    [S08P31A603-412] Update: pose landmark 학습 데이터에 추가
    Former-commit-id: 4eb892cb77230f60682425f40bcfdaa4ba70ac34

--- backend/hearo_fastapi/ai_code/sl_recognition/utils.py
+++ backend/hearo_fastapi/ai_code/sl_recognition/utils.py
@@ -16,7 +16,33 @@ def get_words_list_from_router():
     return words
 
 
-async def joint_to_angle(joint):
+def joint_to_angle(joint):
+    # Compute angles between joints
+    v1 = joint[[0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0, 17, 18, 19,], :3,]  # Parent joint
+    v2 = joint[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,], :3,]  # Child joint
+    v = v2 - v1  # [20, 3]
+
+    # Normalize v
+    v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+    # Get angle using arcos of dot product
+    angle = np.arccos(
+        np.einsum(
+            "nt,nt->n",
+            v[[0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18], :],
+            v[[1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 14, 15, 17, 18, 19], :],
+        )
+    )  # (15,)
+
+    # Convert radian to degree
+    # angle = np.degrees(angle)  # 학습 저해하여 제외
+
+    angle = np.array(angle, dtype=np.float32)
+
+    return angle
+
+
+async def joint_to_angle_from_router(joint):
     # Compute angles between joints
     v1 = joint[[0, 1, 2, 3, 0, 5, 6, 7, 0, 9, 10, 11, 0, 13, 14, 15, 0, 17, 18, 19,], :3,]  # Parent joint
     v2 = joint[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,], :3,]  # Child joint
@@ -44,4 +70,4 @@ async def joint_to_angle(joint):
 
 def time_to_string(timestamp):
     time_obj = time.localtime(timestamp)
-    return time.strftime("%y%m%d%H%M%S", time_obj)
+    return time.strftime("%y%m%d%H%M", time_obj)
