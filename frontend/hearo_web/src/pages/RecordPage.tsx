7da57b876ad8c55bd314e7695d538221b9424a27
Namdarun <gs20wow@gmail.com>
2023-05-11 02:24:28 +0900

    [S08P31A603-362] Feat : 기록페이지 props
    상세설명
    -
    
    
    Former-commit-id: 40c821e7f2f51ef1ccf8d5aedb2325d0d685f68e

--- frontend/hearo_web/src/pages/RecordPage.tsx
+++ frontend/hearo_web/src/pages/RecordPage.tsx
@@ -1,18 +1,20 @@
 import { TrashIcon } from "@heroicons/react/24/solid";
-import { NewspaperIcon } from "@heroicons/react/24/outline";
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
 import { useNavigate, useLocation } from "react-router-dom";
 import { RemoveRecordModal } from "@/components";
 import React, { useState } from "react";
 import {RecordpageSideBar,
 } from "@/components";
 
-interface LocationState {
-  title: string;
+interface RecordPageProps {
+  title?: string;
   date: string;
   description: string;
+  onChangeTitle: (title: string) => void;
 }
 
-function RecordPage() {
+
+function RecordPage({ onChangeTitle }: RecordPageProps) {
   const location = useLocation();
   const navigate = useNavigate();
   const state = location.state as { title: string, date: string, description : string}
@@ -20,11 +22,23 @@ function RecordPage() {
 
   const [title, setTitle] = useState(initialTitle);
   const [openRemoveRecordModal, setOpenRemoveRecordModal] = useState<boolean>(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+  const [updatedTitle, setUpdatedTitle] = useState('');
 
+  const [newTitle, setNewTitle] = useState<string>(title || "");
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value);
+    e.preventDefault()
+    const newTitle = e.target.value;
+    setTitle(newTitle);
+    setUpdatedTitle(newTitle);
+    console.log(newTitle)
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onChangeTitle(newTitle);
+  };
 
   const moveToRecords = () => {
     navigate(`/records`);
@@ -35,45 +49,57 @@ function RecordPage() {
     // Record 삭제 코드 추가
   };
 
+
+
   return (
     <div>
       <RecordpageSideBar/>
         <div className="fixed right-0 mt-20 w-[82%]">
           <div className="mx-8"> 
             <div className="flex justify-between items-stretch ">
+              <div className="flex flex-row items-center">
                 <div className="font-bold text-3xl text-gray-600 rounded-lg self-center">
+                  <form onSubmit={handleSubmit}>
                   <input
-                    type="text" 
+                    type="text"
                     value={title}
                     onChange={handleTitleChange}
-                    className="
-                      w-full
-                      hover:cursor-pointer 
-                      border-none 
-                      border-black
-                      hover:border-blue-main 
-                      hover:shadow-blue-main 
-                      text-black
-                      "
-                  />
-                </div>
-                <div className="flex flex-row  mr-4">
-                    <div className="m-4 pl-0 p-1">
-                      <button
-                        className="px-4 py-2 bg-red-main text-white rounded-full hover:bg-red-400 transition-all duration-200 ease-out"
-                        onClick={moveToRecords}
-                        >
-                        Back
-                      </button>
+                    placeholder="제목을 입력해주세요"
+                    className="p-2 w-full hover:cursor-pointer hover:outline rounded-lg"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setIsFocused(false)}
+                    />
+                  </form>
+                </div>  
+                {(isHovered || isFocused) ? (
+                  <div className="ml-4 w-10 h-10 self-center text-gray-600">
+                    <PencilSquareIcon/>
+                  </div>
+                ) : title ? null : (
+                  <div className="ml-4 w-10 h-10 self-center text-gray-600">
+                    <PencilSquareIcon/>
+                  </div>
+                )}
+              </div>
+              <div className="flex flex-row  mr-4">
+                  <div className="m-4 p-1">
+                    <button
+                      className="w-20 py-2 bg-red-main text-white rounded-full hover:bg-red-400 transition-all duration-200 ease-out"
+                      onClick={moveToRecords}
+                      >
+                      Back
+                    </button>
+                  </div>
+                  <div
+                    className="mx-2 my-4 font-semibold text-red-main rounded-full bg-red-50 w-12 h-12 hover:bg-red-300 hover:shadow-sm transition-all duration-200 ease-out flex justify-center items-center"
+                    onClick={() => setOpenRemoveRecordModal(true)}>
+                    <div className="w-6 h-6">
+                      <TrashIcon />
                     </div>
-                    <div
-                      className="m-4 font-semibold text-red-main rounded-full bg-red-50 w-12 h-12 hover:bg-red-300 hover:shadow-sm transition-all duration-200 ease-out flex justify-center items-center"
-                      onClick={() => setOpenRemoveRecordModal(true)}>
-                      <div className="w-6 h-6">
-                        <TrashIcon />
-                      </div>
-                    </div>
-                </div>
+                  </div>
+              </div>
             </div>
             <div className="text-sm">{date}</div>
             <div className="my-2">{description}</div>
