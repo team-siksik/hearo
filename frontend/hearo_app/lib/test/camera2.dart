730812aeb0eb7fb069d3855b57ae594393379057
cjfal2 <cjfal2@kakao.com>
2023-05-12 00:42:21 +0900

    [S08P31A603-000] Feat: 유사무한소켓 영상 오디오
    상세설명
    - 유사무한 소켓

---
+++ frontend/hearo_app/lib/test/camera2.dart
@@ -0,0 +1,160 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:hearo_app/skills/socket_overall.dart';
+import 'package:image/image.dart' as img;
+
+/// Camera2는 메인 애플리케이션입니다.
+class Camera2 extends StatefulWidget {
+  /// 기본 생성자
+  const Camera2({Key? key}) : super(key: key);
+
+  @override
+  State<Camera2> createState() => _Camera2State();
+}
+
+class _Camera2State extends State<Camera2> {
+  final SocketOverall videoSocket = SocketOverall();
+  late List<CameraDescription> cameras;
+  late CameraController cameraController;
+  bool isStreamingImages = false;
+
+  @override
+  void initState() {
+    videoSocket.connect();
+    videoSocket.enterRoom("1111");
+
+    availableCameras().then((availableCameras) {
+      cameras = availableCameras;
+      late CameraDescription frontCamera;
+      for (final camera in cameras) {
+        if (camera.lensDirection == CameraLensDirection.front) {
+          frontCamera = camera;
+          break;
+        }
+      }
+      cameraController = CameraController(frontCamera, ResolutionPreset.low);
+
+      cameraController.initialize().then((_) {
+        if (!mounted) {
+          return;
+        }
+        setState(() {});
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    cameraController.dispose();
+    videoSocket.closeRoom("1111");
+    videoSocket.disconnect();
+    super.dispose();
+  }
+
+  void startImageStream() async {
+    if (!isStreamingImages) {
+      setState(() {
+        isStreamingImages = true;
+      });
+      cameraController.startImageStream((CameraImage availableImage) {
+        if (isStreamingImages) {
+          processCameraImage(availableImage);
+        }
+      });
+    }
+  }
+
+  void startSend() {
+    setState(() {
+      isStreamingImages = true;
+    });
+    Timer timer = timeSend();
+
+    // 일정 시간(예: 10초) 후에 함수 멈추기
+    Timer(Duration(seconds: 10), () {
+      stopFunction(timer);
+    });
+  }
+
+  Timer timeSend() {
+    return Timer.periodic(Duration(milliseconds: (1000 ~/ 30)), (timer) {
+      // 1초에 30번 실행되는 함수 호출
+      takePicture();
+    });
+  }
+
+  void takePicture() async {
+    try {
+      final image = await cameraController.takePicture();
+      final File file = File(image.path);
+      final bytes = await file.readAsBytes();
+      final encodedImage = base64.encode(bytes);
+      videoSocket.sendVideo("1111", encodedImage);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void stopFunction(Timer timer) {
+    setState(() {
+      isStreamingImages = false;
+    });
+    timer.cancel();
+    print('함수가 멈추었습니다.');
+  }
+
+  void stopImageStream() {
+    if (isStreamingImages) {
+      setState(() {
+        isStreamingImages = false;
+      });
+      cameraController.stopImageStream();
+    }
+  }
+
+  Future<void> processCameraImage(CameraImage image) async {
+    final img.Image convertedImage = img.Image.fromBytes(
+      width: image.planes[0].width as int,
+      height: image.planes[0].height as int,
+      bytes: image.planes[0].bytes as ByteBuffer,
+    );
+
+    final List<int> imageBytes = img.encodePng(convertedImage);
+    final String base64Image = base64Encode(imageBytes);
+
+    videoSocket.sendVideo('1111', base64Image);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!cameraController.value.isInitialized) {
+      return Container();
+    }
+    return Scaffold(
+      appBar: AppBar(title: Text("영상 소켓 테스트")),
+      body: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 1 / 1,
+            child: CameraPreview(cameraController),
+          ),
+          ElevatedButton(
+            onPressed: takePicture,
+            child: Text('Take Picture'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: isStreamingImages ? stopImageStream : startSend,
+        child: Icon(isStreamingImages ? Icons.stop : Icons.videocam),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
+    );
+  }
+}
--- frontend/hearo_app/lib/test/socket_test.dart
+++ frontend/hearo_app/lib/test/socket_test.dart
@@ -1,11 +1,10 @@
-import 'dart:typed_data';
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
-import 'package:hearo_app/skills/socket_overall.dart';
 import 'package:path_provider/path_provider.dart';
-import 'dart:convert';
-import 'package:flutter/services.dart' show rootBundle;
+import 'package:hearo_app/skills/socket_overall.dart';
 
 class SocketTest extends StatefulWidget {
   const SocketTest({super.key});
@@ -15,27 +14,12 @@ class SocketTest extends StatefulWidget {
 }
 
 class _SocketTestState extends State<SocketTest> {
-  Future<String> pngToBase64(String imageName) async {
-    ByteData imageBytes = await rootBundle.load('assets/images/$imageName.png');
-    List<int> bytes = imageBytes.buffer.asUint8List();
-    String base64Image = base64Encode(bytes);
-    print(base64Image.length);
-    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-    return base64Image;
-  }
-
-  Future<void> someFunction() async {
-    String imageName = 'test';
-    String img64 = await pngToBase64(imageName);
-    print("@@");
-    audioSocket.sendVideo('1111', img64);
-  }
-
   final SocketOverall audioSocket = SocketOverall();
-  List messages = [];
   final FlutterSoundRecorder _audioRecorder = FlutterSoundRecorder();
   final FlutterSoundPlayer _audioPlayer = FlutterSoundPlayer();
   late String _recordingFilePath;
+  bool _isRecording = false;
+  List temp = [];
   @override
   void initState() {
     super.initState();
@@ -57,7 +41,7 @@ class _SocketTestState extends State<SocketTest> {
 
   Future<void> _initializeAudioRecorder() async {
     await _audioRecorder.openRecorder();
-    _audioRecorder.setSubscriptionDuration(Duration(milliseconds: 10));
+    // _audioRecorder.setSubscriptionDuration(Duration(milliseconds: 100));
   }
 
   Future<void> _initializeAudioPlayer() async {
@@ -66,11 +50,16 @@ class _SocketTestState extends State<SocketTest> {
 
   Future<void> _startRecording() async {
     try {
+      setState(() {
+        _isRecording = true;
+      });
       final tempDir = await getTemporaryDirectory();
       final recordingFileName =
           'recording_${DateTime.now().microsecondsSinceEpoch}.wav';
       final recordingFilePath = '${tempDir.path}/$recordingFileName';
-      await _audioRecorder.startRecorder(toFile: recordingFilePath);
+      await _audioRecorder.startRecorder(
+          toFile: recordingFilePath, codec: Codec.pcm16WAV);
+
       setState(() {
         _recordingFilePath = recordingFilePath;
       });
@@ -81,18 +70,41 @@ class _SocketTestState extends State<SocketTest> {
 
   Future<void> _stopRecording() async {
     try {
-      await _audioRecorder.stopRecorder();
+      _audioRecorder.stopRecorder();
+      File file = File(_recordingFilePath);
+      List<int> fileData = file.readAsBytesSync();
+      String fileDataB64 = base64Encode(fileData);
+      audioSocket.sendAudio('1111', fileDataB64);
+      audioSocket.socket.on(
+        "audio",
+        (data) {
+          temp.add(data);
+          print(data);
+        },
+      );
     } catch (e) {
       print(e.toString());
     }
   }
 
-  Future<void> _playRecording() async {
-    try {
-      await _audioPlayer.startPlayer(fromURI: _recordingFilePath);
-    } catch (e) {
-      print(e.toString());
+  void _recordAndSendEverySecond() async {
+    setState(() {
+      _isRecording = true;
+    });
+
+    while (_isRecording) {
+      _startRecording();
+      await Future.delayed(Duration(microseconds: 1000));
+      _stopRecording();
     }
+  }
+
+  void _stopSendRecording() async {
+    setState(() {
+      _isRecording = false;
+    });
+    await _audioRecorder.stopRecorder();
+    await _audioRecorder.closeRecorder();
   }
 
   @override
@@ -107,52 +119,21 @@ class _SocketTestState extends State<SocketTest> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextButton(
-                    onPressed: () {
-                      audioSocket.connect();
-                    },
-                    child: Text("소켓연결")),
-                TextButton(
-                    onPressed: () {
-                      audioSocket.enterRoom('1111');
-                    },
-                    child: Text("소켓방드가기")),
-                TextButton(
-                  onPressed: () {
-                    someFunction();
-                  },
-                  child: Text("이미지전송"),
-                ),
-                TextButton(
-                    onPressed: () {
-                      audioSocket.sendMessageToRoom("1111", "어뜨케된겨");
-                    },
-                    child: Text("메시지전송")),
-                TextButton(
-                    onPressed: () {
-                      audioSocket.onMessage(
-                        (p0) {},
-                      );
-                    },
-                    child: Text("메시지확인")),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
                 IconButton(
-                  onPressed: _startRecording,
+                  onPressed: _recordAndSendEverySecond,
                   icon: Icon(Icons.mic),
                 ),
                 SizedBox(width: 10),
                 IconButton(
-                  onPressed: _stopRecording,
+                  onPressed: _stopSendRecording,
                   icon: Icon(Icons.stop),
                 ),
                 SizedBox(width: 10),
                 IconButton(
-                  onPressed: _playRecording,
-                  icon: Icon(Icons.play_arrow),
+                  onPressed: () {
+                    print(temp);
+                  },
+                  icon: Icon(Icons.text_increase_rounded),
                 ),
               ],
             ),
