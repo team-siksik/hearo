a0d17033f169aa85658e6f780934b87d9ee91def
nayeonkinn <nayeonkinn@gmail.com>
2023-05-11 16:15:14 +0900

    [S08P31A603-432] Feat: S3 파일 업로드 성공
    Former-commit-id: 9a429f8d55f1eaa0a9b8a0c020a0b7e0e25b0ac2

--- backend/hearo/src/main/java/com/ssafy/hearo/global/config/s3/S3Config.java
+++ backend/hearo/src/main/java/com/ssafy/hearo/global/config/s3/S3Config.java
@@ -1,8 +1,9 @@
 package com.ssafy.hearo.global.config.s3;
 
+import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,17 +14,21 @@ import org.springframework.context.annotation.Configuration;
 public class S3Config {
     @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
+
     @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
+
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
-    public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+    public AmazonS3 amazonS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
                 .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
                 .build();
     }
 }
