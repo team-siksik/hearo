55e3343e6e2130aee064049d1e5c6746ec2fba25
nayeonkinn <nayeonkinn@gmail.com>
2023-05-12 11:22:15 +0900

    [S08P31A603-433] Fix: 사라졌던 Swagger 설정 다시 추가
    Former-commit-id: ab4c7f910327abfb4bbb2ccee500f79f80e6ad18

---
+++ backend/hearo/src/main/java/com/ssafy/hearo/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.ssafy.hearo.global.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Bean
+    public Docket restAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ssafy.hearo"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Hearo Spring Boot REST API")
+                .version("0.1.0")
+                .description("소리를 잇는 다리, Hearo의 swagger api입니다.")
+                .build();
+    }
+}
