7e9f6be1b31e4902ad77f317bc8a4f2564a1800e
Namdarun <gs20wow@gmail.com>
2023-05-12 17:54:47 +0900

    [S08P31A603-362] merge
    상세설명
    -
    
    
    Former-commit-id: 25be5f2bb18bc1b133c6d55d480c340c4399ac47

--- frontend/hearo_web/src/components/common/ui/Dropdown.tsx
+++ frontend/hearo_web/src/components/common/ui/Dropdown.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 interface DropDownProps {
   gender: string[];
   showDropDown: boolean;
   toggleDropDown: Function;
   genderSelection: Function;
-};
+}
 
-function DropDown ({gender, genderSelection}: DropDownProps) {
+function DropDown({ gender, genderSelection }: DropDownProps) {
   const [showDropDown, setShowDropDown] = useState<boolean>(false);
 
   /**
-   * 젠더 설정 
+   * 젠더 설정
    * 부모 컴포넌트로 이동
    *
    * @param gender
@@ -26,25 +26,23 @@ function DropDown ({gender, genderSelection}: DropDownProps) {
 
   return (
     <>
-      <div className={showDropDown ? 'dropdown py-2' : 'dropdown active mt-2'}>
-        {gender.map(
-          (gender: string, index: number): JSX.Element => {
-            return (
-              <p
-                className='hover:bg-blue-300 rounded-xl py-2'
-                key={index}
-                onClick={(): void => {
-                  onClickHandler(gender);
-                }}
-              >
-                {gender}
-              </p>
-            );
-          }
-        )}
+      <div className={showDropDown ? "dropdown py-2" : "dropdown active mt-2"}>
+        {gender.map((gender: string, index: number): JSX.Element => {
+          return (
+            <p
+              className="rounded-xl py-2 hover:bg-blue-300"
+              key={index}
+              onClick={(): void => {
+                onClickHandler(gender);
+              }}
+            >
+              {gender}
+            </p>
+          );
+        })}
       </div>
     </>
   );
-};
+}
 
 export default DropDown;
