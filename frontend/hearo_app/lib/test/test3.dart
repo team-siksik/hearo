843a9670f53613da7ab8ee87191882ce04fc684a
cjfal2 <cjfal2@kakao.com>
2023-05-12 11:18:46 +0900

    [S08P31A603-000] Feat: 아이콘설정 블루투스 설정
    상세설명
    - 아이콘 설정 블루투스 설정
    
    
    Former-commit-id: 4bc775623d42f865a28f8c208cfa6b437ae0401f

--- frontend/hearo_app/lib/test/test3.dart
+++ frontend/hearo_app/lib/test/test3.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
@@ -7,65 +5,132 @@ class Test3 extends StatefulWidget {
   const Test3({super.key});
 
   @override
-  State<Test3> createState() => _Test3State();
+  _Test3State createState() => _Test3State();
 }
 
 class _Test3State extends State<Test3> {
+  static const String SERVICE_UUID = "00001101-0000-1000-8000-00805f9b34fb";
+  static const String CHARACTERISTIC_UUID =
+      "00001101-0000-1000-8000-00805f9b34fb";
+
   FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
-  late BluetoothDevice hc06Device;
-  late BluetoothCharacteristic writeCharacteristic;
+  BluetoothDevice? connectedDevice;
+  Stream<List<int>>? receiveStream;
+  String receivedData = "";
 
   @override
   void initState() {
     super.initState();
-    _initBluetooth();
+    flutterBlue.state.listen((state) {
+      if (state == BluetoothState.on) {
+        scanDevices();
+      }
+    });
   }
 
-  void _initBluetooth() async {
-    await flutterBlue.startScan(timeout: Duration(seconds: 4));
+  void scanDevices() {
+    flutterBlue.startScan(timeout: Duration(seconds: 4));
 
     flutterBlue.scanResults.listen((results) {
       for (ScanResult result in results) {
-        if (result.device.name == 'HC-06') {
-          hc06Device = result.device;
+        if (result.device.name.contains("HC-06")) {
+          connectToDevice(result.device);
         }
       }
     });
-
-    flutterBlue.stopScan();
-    _connectToDevice();
   }
 
-  void _connectToDevice() async {
-    await hc06Device.connect();
-    List<BluetoothService> services = await hc06Device.discoverServices();
+  void connectToDevice(BluetoothDevice device) async {
+    flutterBlue.stopScan();
 
-    for (var service in services) {
-      for (var characteristic in service.characteristics) {
-        if (characteristic.uuid.toString() ==
-            '0000ffe1-0000-1000-8000-00805f9b34fb') {
-          writeCharacteristic = characteristic;
+    await device.connect(autoConnect: false);
+
+    List<BluetoothService> services = await device.discoverServices();
+
+    for (BluetoothService service in services) {
+      if (service.uuid.toString() == SERVICE_UUID) {
+        print("checked1");
+        for (BluetoothCharacteristic characteristic
+            in service.characteristics) {
+          if (characteristic.uuid.toString() == CHARACTERISTIC_UUID) {
+            print("checked2");
+            setState(() {
+              connectedDevice = device;
+              receiveStream = characteristic.value;
+              receiveStream!.listen((data) {
+                setState(() {
+                  receivedData = String.fromCharCodes(data);
+                });
+              });
+            });
+            break;
+          }
+        }
+        break;
+      }
+    }
+  }
+
+  void sendData(String data) async {
+    print(connectedDevice);
+    if (connectedDevice != null) {
+      List<int> dataBytes = data.codeUnits;
+      List<BluetoothService> services =
+          await connectedDevice!.discoverServices();
+
+      for (BluetoothService service in services) {
+        for (BluetoothCharacteristic characteristic
+            in service.characteristics) {
+          if (characteristic.uuid.toString() == CHARACTERISTIC_UUID) {
+            await characteristic.write(dataBytes);
+            return;
+          }
         }
       }
     }
   }
 
-  void _sendData() async {
-    List<int> dataToSend = utf8.encode('Hello, HC-06!');
-
-    await writeCharacteristic.write(dataToSend);
+  void disconnectDevice() async {
+    if (connectedDevice != null) {
+      await connectedDevice!.disconnect();
+      setState(() {
+        connectedDevice = null;
+        receiveStream = null;
+        receivedData = "";
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bluetooth HC-06'),
+        title: Text('Bluetooth 통신'),
       ),
       body: Center(
-        child: TextButton(
-          onPressed: _sendData,
-          child: Text('Send Data'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('수신된 데이터: $receivedData'),
+            ElevatedButton(
+              onPressed: () {
+                scanDevices();
+              },
+              child: Text('기기 스캔'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                sendData("3");
+              },
+              child: Text('데이터 전송'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                disconnectDevice();
+              },
+              child: Text('연결끊기'),
+            ),
+          ],
         ),
       ),
     );
