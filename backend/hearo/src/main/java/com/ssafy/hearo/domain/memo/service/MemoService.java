2b083e8ca0d6abd6b8d5c87ee6a3163140c101df
nayeonkinn <nayeonkinn@gmail.com>
2023-05-14 03:03:33 +0900

    [S08P31A603-433] Feat: 메모 목록 조회 API 구현 및 중복 코드 제거

--- backend/hearo/src/main/java/com/ssafy/hearo/domain/memo/service/MemoService.java
+++ backend/hearo/src/main/java/com/ssafy/hearo/domain/memo/service/MemoService.java
@@ -2,10 +2,14 @@ package com.ssafy.hearo.domain.memo.service;
 
 import com.ssafy.hearo.domain.account.entity.Account;
 import com.ssafy.hearo.domain.memo.dto.MemoRequestDto.*;
+import com.ssafy.hearo.domain.memo.dto.MemoResponseDto.*;
+
+import java.util.List;
 
 
 public interface MemoService {
 
+    List<MemoInfoResponseDto> getMemoList(Account account, Long recordSeq);
     void createMemo(Account account, Long recordSeq, CreateMemoRequestDto requestDto);
     void modifyMemo(Account account, Long recordSeq, Long memoSeq, ModifyMemoRequestDto requestDto);
     void deleteMemo(Account account, Long recordSeq, DeleteMemoRequestDto requestDto);
