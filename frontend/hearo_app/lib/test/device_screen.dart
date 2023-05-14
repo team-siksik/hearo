a44f1e5cfdbf7b8a5455c2b2c3b507fae8f28706
cjfal2 <cjfal2@kakao.com>
2023-05-14 21:20:34 +0900

    [S08P31A603-000] Feat: IoT 마이크 시도 실패 상세설명 - IoT 마이크 시도 실패

--- frontend/hearo_app/lib/test/device_screen.dart
+++ frontend/hearo_app/lib/test/device_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class DeviceScreen extends StatefulWidget {
   DeviceScreen({Key? key, required this.device}) : super(key: key);
-  // 장치 정보 전달 받기
+
   final BluetoothDevice device;
 
   @override
@@ -12,48 +12,30 @@ class DeviceScreen extends StatefulWidget {
 }
 
 class _DeviceScreenState extends State<DeviceScreen> {
-  // flutterBlue
   FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
-
-  // 연결 상태 표시 문자열
   String stateText = 'Connecting';
-
-  // 연결 버튼 문자열
   String connectButtonText = 'Disconnect';
-
-  // 현재 연결 상태 저장용
   BluetoothDeviceState deviceState = BluetoothDeviceState.disconnected;
-
-  // 연결 상태 리스너 핸들 화면 종료시 리스너 해제를 위함
   StreamSubscription<BluetoothDeviceState>? _stateListener;
-
   List<BluetoothService> bluetoothService = [];
-
-  //
   Map<String, List<int>> notifyDatas = {};
-  String test = '';
+
   @override
-  initState() {
+  void initState() {
     super.initState();
-    // 상태 연결 리스너 등록
     _stateListener = widget.device.state.listen((event) {
-      debugPrint('event :  $event');
+      debugPrint('event: $event');
       if (deviceState == event) {
-        // 상태가 동일하다면 무시
         return;
       }
-      // 연결 상태 정보 변경
       setBleConnectionState(event);
     });
-    // 연결 시작
     connect();
   }
 
   @override
   void dispose() {
-    // 상태 리스터 해제
     _stateListener?.cancel();
-    // 연결 해제
     disconnect();
     super.dispose();
   }
@@ -61,115 +43,89 @@ class _DeviceScreenState extends State<DeviceScreen> {
   @override
   void setState(VoidCallback fn) {
     if (mounted) {
-      // 화면이 mounted 되었을때만 업데이트 되게 함
       super.setState(fn);
     }
   }
 
-  /* 연결 상태 갱신 */
-  setBleConnectionState(BluetoothDeviceState event) {
-    switch (event) {
-      case BluetoothDeviceState.disconnected:
-        stateText = 'Disconnected';
-        // 버튼 상태 변경
-        connectButtonText = 'Connect';
-        break;
-      case BluetoothDeviceState.disconnecting:
-        stateText = 'Disconnecting';
-        break;
-      case BluetoothDeviceState.connected:
-        stateText = 'Connected';
-        // 버튼 상태 변경
-        connectButtonText = 'Disconnect';
-        break;
-      case BluetoothDeviceState.connecting:
-        stateText = 'Connecting';
-        break;
-    }
-    //이전 상태 이벤트 저장
-    deviceState = event;
-    setState(() {});
+  void setBleConnectionState(BluetoothDeviceState event) {
+    setState(() {
+      switch (event) {
+        case BluetoothDeviceState.disconnected:
+          stateText = 'Disconnected';
+          connectButtonText = 'Connect';
+          break;
+        case BluetoothDeviceState.disconnecting:
+          stateText = 'Disconnecting';
+          break;
+        case BluetoothDeviceState.connected:
+          stateText = 'Connected';
+          connectButtonText = 'Disconnect';
+          break;
+        case BluetoothDeviceState.connecting:
+          stateText = 'Connecting';
+          break;
+      }
+      deviceState = event;
+    });
   }
 
-  /* 연결 시작 */
   Future<bool> connect() async {
-    Future<bool>? returnValue;
-    setState(() {
-      /* 상태 표시를 Connecting으로 변경 */
-      stateText = 'Connecting';
-    });
+    try {
+      setState(() {
+        stateText = 'Connecting';
+      });
 
-    /* 
-      타임아웃을 15초(15000ms)로 설정 및 autoconnect 해제
-       참고로 autoconnect가 true되어있으면 연결이 지연되는 경우가 있음.
-     */
-    await widget.device
-        .connect(autoConnect: false)
-        .timeout(Duration(milliseconds: 15000), onTimeout: () {
-      //타임아웃 발생
-      //returnValue를 false로 설정
-      returnValue = Future.value(false);
-      debugPrint('timeout failed');
+      await widget.device
+          .connect(autoConnect: false)
+          .timeout(Duration(milliseconds: 15000));
 
-      //연결 상태 disconnected로 변경
-      setBleConnectionState(BluetoothDeviceState.disconnected);
-    }).then((data) async {
+      setState(() {
+        stateText = 'Connected';
+        connectButtonText = 'Disconnect';
+      });
+
       bluetoothService.clear();
-      if (returnValue == null) {
-        //returnValue가 null이면 timeout이 발생한 것이 아니므로 연결 성공
-        debugPrint('connection successful');
-        print('start discover service');
-        List<BluetoothService> bleServices =
-            await widget.device.discoverServices();
-        setState(() {
-          bluetoothService = bleServices;
-        });
-        // 각 속성을 디버그에 출력
-        for (BluetoothService service in bleServices) {
-          print('============================================');
-          print('Service UUID: ${service.uuid}');
-          for (BluetoothCharacteristic c in service.characteristics) {
-            if (c.properties.notify && c.descriptors.isNotEmpty) {
-              // 진짜 0x2902 가 있는지 단순 체크용!
-              for (BluetoothDescriptor d in c.descriptors) {
-                print('BluetoothDescriptor uuid ${d.uuid}');
-                if (d.uuid == BluetoothDescriptor.cccd) {
-                  print('d.lastValue: ${d.lastValue}');
-                }
-              }
+      List<BluetoothService> bleServices =
+          await widget.device.discoverServices();
+      setState(() {
+        bluetoothService = bleServices;
+      });
 
-              // notify가 설정 안되었다면...
-              if (!c.isNotifying) {
-                try {
-                  await c.setNotifyValue(true);
-                  // 받을 데이터 변수 Map 형식으로 키 생성
-                  notifyDatas[c.uuid.toString()] = List.empty();
-                  c.value.listen((value) {
-                    // 데이터 읽기 처리
-                    print(
-                        '${c.uuid}: ${value.runtimeType} @@@@@@@@@@@@@@@@@@@@@');
-                    print(value);
-                    print("#############");
-                    setState(() {
-                      // 받은 데이터 저장 화면 표시용
-                      notifyDatas[c.uuid.toString()] = value;
-                    });
-                  });
-
-                  // 설정 후 일정시간 지연
-                  await Future.delayed(const Duration(milliseconds: 500));
-                } catch (e) {
-                  print('error ${c.uuid} $e');
-                }
-              }
+      for (BluetoothService service in bleServices) {
+        print('============================================');
+        print('Service UUID: ${service.uuid}');
+        for (BluetoothCharacteristic c in service.characteristics) {
+          if (c.properties.notify &&
+              c.descriptors.isNotEmpty &&
+              !c.isNotifying) {
+            try {
+              await c.setNotifyValue(true);
+              notifyDatas[c.uuid.toString()] = List.empty();
+              c.value.listen((value) {
+                print('${c.uuid}: ${value.runtimeType} @@@@@@@@@@@@@@@@@@@@@');
+                print(value);
+                print("#############");
+                setState(() {
+                  notifyDatas[c.uuid.toString()] = value;
+                });
+              });
+              await Future.delayed(const Duration(milliseconds: 500));
+            } catch (e) {
+              print('error ${c.uuid} $e');
             }
           }
         }
-        returnValue = Future.value(true);
       }
-    });
 
-    return returnValue ?? Future.value(false);
+      return true;
+    } catch (e) {
+      print('connection failed: $e');
+      setState(() {
+        stateText = 'Disconnected';
+        connectButtonText = 'Connect';
+      });
+      return false;
+    }
   }
 
   /* 연결 해제 */
