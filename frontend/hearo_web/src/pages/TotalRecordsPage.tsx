c0f1dcede62409861d0f68a232457f93b4fd984c
isabel-noh <nohhyunjeong93@gmail.com>
2023-05-15 10:15:59 +0900

    merge
    Former-commit-id: 229a310d401f086e6bec8ecbdf02717599549131

--- frontend/hearo_web/src/pages/TotalRecordsPage.tsx
+++ frontend/hearo_web/src/pages/TotalRecordsPage.tsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  RecordpageSideBar,
-
-} from "@/components";
+import { RecordpageSideBar } from "@/components";
 import { RecordsItem } from "@/components";
 import axios from "axios";
 import { RecordAPI } from "@/apis/api";
@@ -26,77 +23,83 @@ function TotalRecordsPage() {
   // const [noRecords, setNoRecords] = useState<boolean>(false);
 
   // TODO: API 설정 추가해야함
-  
+
   const accessToken = localStorage.getItem("accessToken");
-  
+
   const [totalRecords, setTotalRecords] = useState<TotalRecord[]>([]);
-  useEffect(() => {
-    RecordAPI.getRecords(accessToken!)
-      .then((response) => {
-        setTotalRecords(response.data);
-        console.log(response.data);
-        console.log(totalRecords)
-        console.log('나오나?');
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  // useEffect(() => {
+  //   RecordAPI.getRecords(accessToken!)
+  //     .then((response) => {
+  //       setTotalRecords(response.data);
+  //       console.log(response.data);
+  //       console.log(totalRecords)
+  //       console.log('나오나?');
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
   // 변경된 타이틀?
   const handleChangeTitle = (recordSeq: number, newTitle: string) => {
-    const newRecords = totalRecords.map(records => {
+    const newRecords = totalRecords.map((records) => {
       if (records.recordSeq === recordSeq) {
         return {
           ...records,
-          title: newTitle
-        }
+          title: newTitle,
+        };
       }
       return records;
     });
     setTotalRecords(newRecords);
   };
 
-  const handleRemoveRecord = (idToDelete:number) => {
-    const updatedRecords = totalRecords.filter((record) => record.recordSeq !== idToDelete);
+  const handleRemoveRecord = (idToDelete: number) => {
+    const updatedRecords = totalRecords.filter(
+      (record) => record.recordSeq !== idToDelete
+    );
     setTotalRecords(updatedRecords);
     setOpenRemoveRecordModal(false);
   };
 
   // TODO: 기록이 없으면 없는 걸로 보여줘야함
   // const showRecords = () => {
-  //   if (noRecords) 
-  //     return ('no record') 
-  //   else 
-  //     return( 
+  //   if (noRecords)
+  //     return ('no record')
+  //   else
+  //     return(
   //       {totalRecords}
   //     )
-  //   }  
-
+  //   }
 
   return (
     <div>
-      <RecordpageSideBar/>
-      <div className="absolute right-0 mt-20 w-[82%] overflow-auto" style={{ paddingTop: "16px" }}>
-        <div className="fixed right-0 top-20 bottom-0 overflow-y-scroll" style={{ width: "calc(82% - 1rem)" }}>
+      <RecordpageSideBar />
+      <div
+        className="absolute right-0 mt-20 w-[82%] overflow-auto"
+        style={{ paddingTop: "16px" }}
+      >
+        <div
+          className="fixed bottom-0 right-0 top-20 overflow-y-scroll"
+          style={{ width: "calc(82% - 1rem)" }}
+        >
           <div className="space-y-4">
             {/* 테스트 */}
-          {totalRecords && 
-          totalRecords.map((records) => (
-            <div key={records.recordSeq}>
-            <h3>{records.title}</h3>
-            <p>{records.preview}</p>
-          </div>
-          ))}
+            {totalRecords &&
+              totalRecords.map((records) => (
+                <div key={records.recordSeq}>
+                  <h3>{records.title}</h3>
+                  <p>{records.preview}</p>
+                </div>
+              ))}
           </div>
         </div>
       </div>
     </div>
-  )  
+  );
 }
 
 export default TotalRecordsPage;
-
 
 // {totalRecords.map((record) => (
 //   <RecordsItem
