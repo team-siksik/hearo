4764d0cac0e41137ad062f096703722ab7caf7f4
isabel-noh <nohhyunjeong93@gmail.com>
2023-05-15 09:23:00 +0900

    merge
    Former-commit-id: cee04b696584a8796ef754fff541eb810751ebf4

--- frontend/hearo_web/src/STT/Transcription.ts
+++ frontend/hearo_web/src/STT/Transcription.ts
@@ -15,3 +15,11 @@ function Transcription() {
 }
 
 export default Transcription;
+
+export const decodeUnicode = (unicodeString: string): string => {
+  const r = /\\u([\d\w]{4})/gi;
+  unicodeString = unicodeString.replace(r, function (match, grp) {
+    return String.fromCharCode(parseInt(grp, 16));
+  });
+  return unescape(unicodeString);
+};
