5c902ea12b845b179c46700f1fdd01d640b63a31
nayeonkinn <nayeonkinn@gmail.com>
2023-05-17 00:16:30 +0900

    Update: 화자 분리 테스트 중
    Former-commit-id: cd9402e68c5cf016bdb6140603a43383f0f02094

--- backend/hearo_ai/ai_code/speaker_diarization/test/public/receiveAudio.js
+++ backend/hearo_ai/ai_code/speaker_diarization/test/public/receiveAudio.js
@@ -1,28 +1,22 @@
-// def receive_audio(ws: WebSocket, output: Optional[Path]):
-//     while True:
-//         message = ws.recv()
-//         print(f"Received: {message}", end="")
-//         if output is not None:
-//             with open(output, "a") as file:
-//                 file.write(message)
-
-
 onmessage = (e) => {
-    const ws = new WebSocket("ws://k8a6031.p.ssafy.io:8000/");
-    
-    ws.onopen = () => {
-      ws.send("Hello from receiveAudio.js");
-    };
-  
-    ws.onmessage = (event) => {
-      postMessage(event.data);
-    };
-  
-    ws.onclose = () => {
-      // WebSocket 연결이 닫힐 때 처리할 내용
-    };
-  
-    ws.onerror = (error) => {
-      // WebSocket 오류 발생 시 처리할 내용
-    };
+  postMessage(e.data.message + " is ready");
+
+  const ws = new WebSocket("ws://k8a6031.p.ssafy.io:7007/");
+
+  ws.onmessage = (event) => {
+    const message = event.data;
+    postMessage(`Received: ${message}`);
   };
+
+  ws.onopen = () => {
+    console.log("receiveAudio opened");
+  };
+
+  ws.onclose = () => {
+    console.log("receiveAudio closed");
+  };
+
+  ws.onerror = (error) => {
+    console.log("receiveAudio error: " + error.message);
+  };
+};
