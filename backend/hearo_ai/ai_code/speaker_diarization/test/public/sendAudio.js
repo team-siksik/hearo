3f8ebf73b1286a9d6431676b297d5511554833c3
nayeonkinn <nayeonkinn@gmail.com>
2023-05-17 00:16:30 +0900

    Update: 화자 분리 테스트 중
    Former-commit-id: 4d61ef11cbfd8c1daccbc8b2f050a6bc96cc7286

--- backend/hearo_ai/ai_code/speaker_diarization/test/public/sendAudio.js
+++ backend/hearo_ai/ai_code/speaker_diarization/test/public/sendAudio.js
@@ -1,38 +1,145 @@
-// def send_audio(ws: WebSocket, source: Text, step: float, sample_rate: int):
-//     # Create audio source
-//     block_size = int(np.rint(step * sample_rate))
-//     source_components = source.split(":")
-//     if source_components[0] != "microphone":
-//         audio_source = src.FileAudioSource(source, sample_rate)
-//     else:
-//         device = int(source_components[1]) if len(source_components) > 1 else None
-//         audio_source = src.MicrophoneAudioSource(sample_rate, block_size, device)
+importScripts("https://unpkg.com/rxjs@^7/dist/bundles/rxjs.umd.min.js");
 
-//     # Encode audio, then send through websocket
-//     audio_source.stream.pipe(
-//         ops.map(utils.encode_audio)
-//     ).subscribe_(ws.send)
+class SimpleQueue {
+  constructor() {
+    this.queue = [];
+  }
 
-//     # Start reading audio
-//     audio_source.read()
+  enqueue(item) {
+    this.queue.push(item);
+  }
 
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    return this.queue.shift();
+  }
+
+  isEmpty() {
+    return this.queue.length === 0;
+  }
+
+  size() {
+    return this.queue.length;
+  }
+}
+
+class MicrophoneAudioSource {
+  constructor(sampleRate, blockSize) {
+    this.uri = "live_recording";
+    this.sampleRate = sampleRate;
+    this.blockSize = blockSize;
+    this.stream = new rxjs.Subject();
+    this._queue = new SimpleQueue();
+    this._micStream = null;
+    this.initialize();
+  }
+
+  initialize() {
+    const channels = 1;
+    const latency = 0;
+
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then((stream) => {
+        const audioContext = new AudioContext();
+        const source = audioContext.createMediaStreamSource(stream);
+        const bufferSize = this.blockSize;
+        const scriptProcessorNode = audioContext.createScriptProcessor(
+          bufferSize,
+          channels,
+          channels
+        );
+
+        source.connect(scriptProcessorNode);
+        scriptProcessorNode.connect(audioContext.destination);
+
+        this._micStream = stream;
+      })
+      .catch((error) => {
+        console.error("Error accessing microphone:", error);
+      });
+  }
+
+  _readCallback(samples, ...args) {
+    this._queue.enqueue(samples.getChannelData(0));
+  }
+
+  read() {
+    if (this._micStream) {
+      const audioTracks = this._micStream.getAudioTracks();
+      if (audioTracks.length > 0) {
+        const audioTrack = audioTracks[0];
+        if (audioTrack.readyState === "live") {
+          const readLoop = () => {
+            while (!this._queue.isEmpty()) {
+              const samples = this._queue.dequeue();
+              this.stream.next(samples);
+            }
+
+            if (audioTrack.readyState !== "live") {
+              this.stream.complete();
+              this.close();
+              return;
+            }
+
+            setTimeout(readLoop, 0);
+          };
+
+          readLoop();
+        }
+      }
+    }
+  }
+
+  close() {
+    if (this._micStream) {
+      const audioTracks = this._micStream.getAudioTracks();
+      audioTracks.forEach((audioTrack) => {
+        audioTrack.stop();
+      });
+      this._micStream.getTracks().forEach((audioTrack) => {
+        audioTrack.stop();
+      });
+    }
+  }
+}
+
+function encodeAudio(waveform) {
+  const buffer = new Uint8Array(waveform.buffer);
+  let binary = "";
+  for (let i = 0; i < buffer.byteLength; i++) {
+    binary += String.fromCharCode(buffer[i]);
+  }
+  const base64 = btoa(binary);
+  return base64;
+}
 
 onmessage = (e) => {
-  const ws = new WebSocket("ws://k8a6031.p.ssafy.io:8000/");
-  
-  ws.onopen = () => {
-    ws.send("Hello from sendAudio.js");
-  };
+  postMessage(e.data.message + " is ready");
 
-  ws.onmessage = (event) => {
-    postMessage(event.data);
+  const ws = new WebSocket("ws://k8a6031.p.ssafy.io:7007/");
+
+  const step = 0.5;
+  const sampleRate = 16000;
+  const blockSize = Math.round(step * sampleRate);
+  const audioSource = new MicrophoneAudioSource(sampleRate, blockSize);
+
+  audioSource.stream.pipe(rxjs.operators.map(encodeAudio)).subscribe((data) => {
+    ws.send(data);
+  });
+  audioSource.read();
+
+  ws.onopen = () => {
+    console.log("sendAudio opened");
   };
 
   ws.onclose = () => {
-    // WebSocket 연결이 닫힐 때 처리할 내용
+    console.log("sendAudio closed");
   };
 
   ws.onerror = (error) => {
-    // WebSocket 오류 발생 시 처리할 내용
+    console.log("sendAudio error: " + error.message);
   };
 };
