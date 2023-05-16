ef79f89deaf140bc21db727120abdce48b0c31c1
Young312886 <hongym0497@gmail.com>
2023-05-17 02:18:39 +0900

    Fix: 블루투스 오류 방지 대책 수립
    Former-commit-id: 9eea26393eadc36ed5c13f9d332b3e9b2961a8a0

--- Iot/sound_detect.py
+++ Iot/sound_detect.py
@@ -2,11 +2,15 @@ import displayio
 bank = {"air_conditioner":"에어컨", "cars":"자동차", "car_horn":"경적", "children_playing":"아이들 소리", "dog_bark":"개 짓음", "drilling":"공사장",
         "engine_idling":"도로","gun_shot":"총소리","jackhammer":"공사장","phone":"벨소리","siren":"경고음","street_music":"노래소리"}
 def sound_detect(feature):
-    feature_str = str(feature.decode())
+    print(feature, "feature임")
+    text = None
     picture = False
-    text = bank.get(feature_str)
-    if text:
-        file_path = f"resource/{feature_str}.bmp"
-        picture = displayio.OnDiskBitmap(open(file_path, "rb"))
+    if feature:
+        feature_str = str(feature.decode())
+        picture = False
+        text = bank.get(feature_str)
+        if text:
+            file_path = f"resource/{feature_str}.bmp"
+            picture = displayio.OnDiskBitmap(open(file_path, "rb"))
     return text, picture
     
