70f395b5a69cad6928df104623241714782a02ca
nayeonkinn <nayeonkinn@gmail.com>
2023-05-17 13:00:12 +0000

    Update: librosa test
    Former-commit-id: 0ed09abb568fad5ca1e67763ecf69c4abe932e66

--- backend/hearo_ai/router/speaker_diarization2.py
+++ backend/hearo_ai/router/speaker_diarization2.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from main import socket_manager, logger
 
 import base64
-import librosa
+# import librosa
 import io
 import numpy as np
 
@@ -17,17 +17,18 @@ async def root():
 
 @socket_manager.on("audio2")
 async def audio(sid, data):
+    logger.info(f"audio: {sid} sent audio")
+
     room_id = data["room_id"]
     audio_data = data["audio"]
 
-    logger.info(f"audio: {sid} sent audio")
     await socket_manager.emit("info", f"{sid} sent audio", room_id)
 
-    binary_data = base64.b64decode(audio_data)
+    # binary_data = base64.b64decode(audio_data)
 
-    buffer = io.BytesIO(binary_data)
-    audio, sr = librosa.load(buffer, sr=None)
-    stft = np.abs(librosa.stft(audio))
-    spectral_centroids = librosa.feature.spectral_centroid(S=stft, sr=sr)
+    # buffer = io.BytesIO(binary_data)
+    # audio, sr = librosa.load(buffer, sr=None)
+    # stft = np.abs(librosa.stft(audio))
+    # spectral_centroids = librosa.feature.spectral_centroid(S=stft, sr=sr)
 
-    logger.info(spectral_centroids)
+    # logger.info(spectral_centroids)
