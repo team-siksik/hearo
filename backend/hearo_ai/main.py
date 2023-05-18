6d085fe4bea7764e5a6a7e5861b9bfa00430ae89
rjpark12 <75202505+janghoonp12@users.noreply.github.com>
2023-05-19 04:33:02 +0900

    fastapi hotfix
    Former-commit-id: f095aba900a1f7b146fe922087aaa4725aaf1b4c

--- backend/hearo_ai/main.py
+++ backend/hearo_ai/main.py
@@ -34,12 +34,12 @@ app.ssl_context = ssl_context
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
-    HTTPSRedirectMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.add_middleware(HTTPSRedirectMiddleware)
 
 from router import common, sl_recognition, sound_classification, speaker_diarization, text_generate
 
