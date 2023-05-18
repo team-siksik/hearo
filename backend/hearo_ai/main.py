ec2284ae44c74493be96d3512c477cdc88b1404f
rjpark12 <75202505+janghoonp12@users.noreply.github.com>
2023-05-19 04:24:24 +0900

    fastapi hotfix
    Former-commit-id: d99b956672b9b3cd506a7931c0341080f0f5a151

--- backend/hearo_ai/main.py
+++ backend/hearo_ai/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_socketio import SocketManager
-
+import ssl
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
 import logging
 import sys
-
 
 # logger 설정
 logger = logging.getLogger("hearo_logger")
@@ -23,19 +23,25 @@ app = FastAPI()
 # socket 설정
 socket_manager = SocketManager(app)
 
+# SSL 컨텍스트 생성
+ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ssl_context.load_cert_chain("/app/ssl_certificate.pem", "/app/ssl_private_key.pem")
+
+# socket_manager에 SSL 컨텍스트 추가
+socket_manager.configure_ssl_context(ssl_context)
+
 # cors 설정
 origins = ["*"]
 app.add_middleware(
     CORSMiddleware,
+    HTTPSRedirectMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
 from router import common, sl_recognition, sound_classification, speaker_diarization, text_generate
-
 
 # 라우터 설정
 common.router.include_router(sl_recognition.router)
