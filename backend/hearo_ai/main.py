eb08d416083bdde6a2d26694e63e26b5db04d8d6
Young312886 <hongym0497@gmail.com>
2023-05-19 09:54:47 +0900

    Fix: localhost 추가
    Former-commit-id: e3340b0d15f04e42fac55808504cce599378bccf

--- backend/hearo_ai/main.py
+++ backend/hearo_ai/main.py
@@ -24,7 +24,7 @@ app = FastAPI()
 socket_manager = SocketManager(app)
 
 # cors 설정
-origins = ["https://k8a603.p.ssafy.io"]
+origins = ["https://k8a603.p.ssafy.io", "http://localhost:5173"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
