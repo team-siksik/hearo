4ed603c6c652c4f8f624a4994fbe3431ff3af133
isabel-noh <nohhyunjeong93@gmail.com>
2023-05-22 20:25:00 +0900

    redux-persist
    Former-commit-id: c101a5e23aac58a2a8a05f069ffac51215b8651b

--- frontend/hearo_web/src/main.tsx
+++ frontend/hearo_web/src/main.tsx
@@ -5,17 +5,21 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/configStore";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+export const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   // <React.StrictMode>
   <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-    {/*redux store*/}
-    <BrowserRouter>
-      {/* react router dom */}
-      <App />
-    </BrowserRouter>
-    {/* </PersistGate> */}
+    <PersistGate loading={null} persistor={persistor}>
+      {/*redux store*/}
+      <BrowserRouter>
+        {/* react router dom */}
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
   // </React.StrictMode>
 );
